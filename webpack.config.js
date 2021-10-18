a17e9c41d6e666d7668300fc2d0bea3ad802c3dc
Nick Carducci <nmcarducci@gmail.com>
2021-10-17 21:31:45 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
+const { ProvidePlugin } = require('webpack');
+
 module.exports = {
-  target: 'webworker'
+  target: 'webworker',
+  mode: "production",
+  resolve:{
+    fallback: { 
+      "assert": require.resolve("assert/"),
+      "buffer": require.resolve("buffer/"),
+      "crypto": require.resolve("crypto-browserify"),
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "path": require.resolve("path-browserify"),
+      "querystring": require.resolve("querystring-es3"),
+      "stream": require.resolve("stream-browserify"),
+      "url": require.resolve("url/"),
+      "util": require.resolve("util/"),
+      "zlib": require.resolve("browserify-zlib")
+    }
+  },
+  plugins: [
+    new ProvidePlugin({ process: 'process/browser' }), // util requires this internally
+  ],
 };
