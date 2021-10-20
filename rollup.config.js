9ac313864ed7bca93f59866743643e6bb5670e4b
Nick Carducci <nmcarducci@gmail.com>
2021-10-20 18:53:36 -0400

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import path from "path";
+import commonjs from "@rollup/plugin-commonjs";
+import autoExternal from "rollup-plugin-auto-external";
+import nodeResolve from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.js",
+  output: {
+    strict: false,
+    file: "dist/index.js",
+    format: "cjs",
+    sourcemap: false
+  },
+  plugins: [
+    autoExternal(),
+    nodeResolve({
+      only: [/^\.{0,2}\//],
+      extensions: [".js", ".ts"],
+      mainFields: ["module", "main"]
+    }),
+    commonjs({
+      include: ["node_modules/**"],
+      exclude: ["node_modules/process-es6/**"]
+    })
+  ]
+};
