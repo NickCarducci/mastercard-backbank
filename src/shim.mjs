ff3fc1cef471d756c553957289b4e6709c5434ff
Nick Carducci <nmcarducci@gmail.com>
2021-10-22 02:52:00 -0400

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -1,4 +1,18 @@
 import bundle from 'index.js'
 const handlers = bundle.handlers
 
-export { handlers as default }
+const app = { 
+  "assert": require.resolve("assert/"),
+  "buffer": require.resolve("buffer/"),
+  "crypto": require.resolve("crypto-browserify"),
+  "http": require.resolve("stream-http"),
+  "https": require.resolve("https-browserify"),
+  "path": require.resolve("path-browserify"),
+  "querystring": require.resolve("querystring-es3"),
+  "stream": require.resolve("stream-browserify"),
+  "url": require.resolve("url/"),
+  "util": require.resolve("util/"),
+  "zlib": require.resolve("browserify-zlib")
+}
+
+export { handlers as default,  ...app}
