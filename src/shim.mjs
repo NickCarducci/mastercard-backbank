437e3925b9700635d63c6f612e45da63d59afd1b
Nick Carducci <nmcarducci@gmail.com>
2021-10-22 20:01:50 -0400

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -14,19 +14,6 @@ const url = require('url/');
 const util = require('util/');
 const zlib = require("browserify-zlib");
 
-const app = { 
-  "assert": require.resolve("assert/"),
-  "buffer": require.resolve("buffer/"),
-  "crypto": require.resolve("crypto-browserify"),
-  "http": require.resolve("stream-http"),
-  "https": require.resolve("https-browserify"),
-  "path": require.resolve("path-browserify"),
-  "querystring": require.resolve("querystring-es3"),
-  "stream": require.resolve("stream-browserify"),
-  "url": require.resolve("url/"),
-  "util": require.resolve("util/"),
-  "zlib": require.resolve("browserify-zlib")
-}
 
 /*export class Stripe {
     checkout: typeof checkout
@@ -50,15 +37,17 @@ const app = {
         this.checkout.client = client.request
     }
 }*/
-export { handlers as default,
-  assert: require.resolve("assert/"),
-  buffer: require.resolve("buffer/"),
-  crypto: require.resolve("crypto-browserify"),
-  http: require.resolve("stream-http"),
-  https: require.resolve("https-browserify"),
-  path: require.resolve("path-browserify"),
-  querystring: require.resolve("querystring-es3"),
-  stream: require.resolve("stream-browserify"),
-  url: require.resolve("url/"),
-  util: require.resolve("util/"),
-  zlib: require.resolve("browserify-zlib") }
+export { 
+  handlers as default,
+  assert,
+  buffer,
+  crypto,
+  http,
+  https,
+  path,
+  querystring,
+  stream,
+  url,
+  util,
+  zlib
+}
