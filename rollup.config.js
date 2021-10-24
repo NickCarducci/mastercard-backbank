3c4b0cfd89acf8be9b7577230a6ea950b9dde53f
Nick Carducci <nmcarducci@gmail.com>
2021-10-24 14:53:03 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,30 @@
-import path from "path";
+import { terser } from 'rollup-plugin-terser'
 import commonjs from "@rollup/plugin-commonjs";
-import autoExternal from "rollup-plugin-auto-external";
+//import autoExternal from "rollup-plugin-auto-external";
 import nodeResolve from "@rollup/plugin-node-resolve";
 
 export default {
   input: "src/index.js",
   output: {
-    name: "app",
+    exports: 'named',
+    //name: "app",
     strict: false,
-    file: "dist/index.js",
-    format: "iife",
-    sourcemap: false
+    file: "dist/index.mjs",
+    format: "es",
+    sourcemap: true
   },
   plugins: [
-    autoExternal(),
-    nodeResolve({
-      only: [/^\.{0,2}\//],
-      extensions: [".js", ".ts"],
-      mainFields: ["module", "main"]
-    }),
+    //autoExternal(),
     commonjs({
       include: ["node_modules/**"],
       exclude: ["node_modules/process-es6/**"]
-    })
+    }),
+    nodeResolve({
+      browser: true,
+      //only: [/^\.{0,2}\//],
+      //extensions: [".js", ".ts"],
+      //mainFields: ["module", "main"]
+    }),
+    terser()
   ]
 };
