d0ee0ab9181c07502a4d20b4f8c3dcc620b993df
Nick Carducci <nmcarducci@gmail.com>
2021-10-24 03:02:44 -0400

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -50,8 +50,14 @@ const zlib = require("browserify-zlib");
         this.checkout.client = client.request
     }
 }*/
+var work = require('webworkify');
+var worker = work(handlers);//require('./worker.js')
+/*w.addEventListener('message', function (ev) {
+    console.log(ev.data);
+});*/
+//w.postMessage(4); // send the worker a message
 export { 
-  handlers as default,
+  worker as default,
   assert,
   buffer,
   crypto,
