5b436ec77e29ad36f2ebaf5dc8d749e9afe4e53e
Nick Carducci <nmcarducci@gmail.com>
2021-10-27 10:13:28 -0400

    Create browseri.js

---
+++ src/browseri.js
@@ -0,0 +1,22 @@
+import locs from "mastercard-locations";
+import places from "mastercard-places";
+import crs from "cors";
+const cors = crs({
+  origin: true,
+  allowedHeaders: [
+    "Access-Control-Allow-Origin",
+    "Access-Control-Allow-Methods",
+    "Content-Type",
+    "Origin",
+    "X-Requested-With",
+    "Accept"
+  ],
+  methods: ["POST", "OPTIONS"],
+  credentials: true
+});
+
+export {
+  locs,
+  places,
+  crs
+}
