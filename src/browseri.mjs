1b17229777aece6c48433f4cea24769354315a33
Nick Carducci <nmcarducci@gmail.com>
2021-10-27 20:47:45 -0400

    Update browseri.mjs

--- src/browseri.mjs
+++ src/browseri.mjs
@@ -2,8 +2,12 @@ import locs from "mastercard-locations";
 import places from "mastercard-places";
 import crs from "cors";
 
-export default {
-  locs,
-  places,
-  crs
+export default class {
+  Browseri() {
+    return {
+      locs,
+      places,
+      crs
+    }
+  }
 }
