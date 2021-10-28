0b34821059ac385f3cee49ec29a90c2229abf4ca
Nick Carducci <nmcarducci@gmail.com>
2021-10-27 20:11:01 -0400

    Update browseri.mjs

--- src/browseri.mjs
+++ src/browseri.mjs
@@ -1,22 +1,9 @@
 import locs from "mastercard-locations";
 import places from "mastercard-places";
 import crs from "cors";
-const cors = crs({
-  origin: true,
-  allowedHeaders: [
-    "Access-Control-Allow-Origin",
-    "Access-Control-Allow-Methods",
-    "Content-Type",
-    "Origin",
-    "X-Requested-With",
-    "Accept"
-  ],
-  methods: ["POST", "OPTIONS"],
-  credentials: true
-});
 
 export default {
   locs,
   places,
-  cors
+  crs
 }
