aa97f11b07ee43598afe3d2443587f85fb2c50a5
Nick Carducci <nmcarducci@gmail.com>
2021-11-24 04:49:34 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,6 @@
 //importScripts('./require.js');
 //https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js
+import m from "browserii.mjs";
 export class DurableObjectExample {
   constructor(el, env) {
     console.log(el.textContent, "- From the example module");
@@ -23,10 +24,14 @@ export class DurableObjectExample {
   async fetch(req, env) {
     const dataHead = {
       "Content-Type": "application/json"
-    };
+    }; /*,
+        paths: {
+            "src": "src/index.js"
+        },
+        waitSeconds: 15*/
 
     //return fetch(sentryUrl, { body: JSON.stringify(b), method: 'POST' })
-    var require = null;
+    /*var require = null;
     return await import("./require.js").then(async obj => {
       require = await obj.require
       if (!require) {
@@ -41,156 +46,150 @@ export class DurableObjectExample {
       } else {
         require.config({
           baseUrl:
-            "src" /*,
-        paths: {
-            "src": "src/index.js"
-        },
-        waitSeconds: 15*/
-        });
-        //https://stackoverflow.com/questions/35902490/requirejs-difference-between-global-require-and-module-require
-        return require(["browserii.mjs"], async (m) => {
-          //This function will be called when all the dependencies
-          //listed above are loaded. Note that this function could
-          //be called before the page is loaded.
-          //This callback is optional.
-          const { locs, places, crs } = m.Browseri();
-          var iMCard = null,
-            mc = null;
-          const initializeMCard = () => {
-            if (!iMCard) {
-              console.log("initializing mastercard api");
-              mc = locs.MasterCardAPI;
-              iMCard = true;
-              mc.init({
-                sandbox: secrets.NODE_ENV !== "production",
-                authentication: new mc.OAuth(
-                  secrets.MASTERCARD_CONSUMER_KEY,
-                  Buffer.from(secrets.MASTERCARD_P12_BINARY, "base64"),
-                  "keyalias",
-                  "keystorepassword"
-                )
-              });
-            }
-          };
-          const mastercardRoute = async (req, func) => {
-            const cb = (error, data) => (error ? error : data);
-            initializeMCard();
-            let rs = null;
-            if (func === "getAtms") {
-              const {
-                PageLength, //"5"
-                PostalCode, //"11101"
-                PageOffset //"0"
-              } = req.body; //query
-              rs = await locs.ATMLocations.query(
-                {
-                  PageLength,
-                  PostalCode,
-                  PageOffset
-                },
-                cb
-              );
-            } else if (func === "getMerchants") {
-              const { countryCode, latitude, longitude, distance } = req.body; //query
-              const q = {
-                pageOffset: 0,
-                pageLength: 10,
-                radiusSearch: "true",
-                unit: "km",
-                distance,
-                place: {
-                  countryCode,
-                  latitude,
-                  longitude
-                }
-              };
-              rs = await places.MerchantPointOfInterest.create(q, cb);
-            } else if (func === "getNames") {
-              rs = await places.MerchantCategoryCodes.query({}, cb);
-            } else if (func === "getTypes") {
-              rs = await places.MerchantIndustries.query({}, cb);
-            }
-            return rs && rs;
-          };
-          const cors = crs({
-            origin: true,
-            allowedHeaders: [
-              "Access-Control-Allow-Origin",
-              "Access-Control-Allow-Methods",
-              "Content-Type",
-              "Origin",
-              "X-Requested-With",
-              "Accept"
-            ],
-            methods: ["POST", "OPTIONS"],
-            credentials: true
-          });
-          //return async handleRequest(request)async (req) => {
-          var origin = req.get("Origin");
-          var allowedOrigins = ["https://vau.money", "https://jwi5k.csb.app"];
-          if (allowedOrigins.indexOf(origin) > -1) {
-            // Origin Allowed!!
-            if (req.method === "OPTIONS") {
-              // Method accepted for next request
-              return new Response(
-                {},
-                {
-                  status: "200",
-                  statusText:
-                    "successful header check for POST process: " + req.url,
-                  headers: {
-                    ...dataHead,
-                    "Access-Control-Allow-Methods": "POST"
-                  }
-                }
-              );
-            } else {
-              let rs = null;
-              if (req.url === "/deposit") {
-                rs = await mastercardRoute(req, "getAtms");
-              } else if (req.url === "/merchant_names") {
-                rs = await mastercardRoute(req, "getNames");
-              } else if (req.url === "/merchant_types") {
-                rs = await mastercardRoute(req, "getTypes");
-              } else if (req.url === "/merchants") {
-                rs = await mastercardRoute(req, "getMerchants");
-              }
-              if (rs) {
-                //isBase64Encoded: false,
-
-                return new Response(
-                  {
-                    data: rs
-                  },
-                  {
-                    status: "200",
-                    message: "success: " + req.url,
-                    headers: dataHead
-                  }
-                );
-              } else {
-                return new Response(
-                  {},
-                  {
-                    status: "500",
-                    message: "no success doof: " + req.url,
-                    headers: dataHead
-                  }
-                );
-              }
-            }
-          } else
-            return new Response(
-              {},
-              {
-                status: "400",
-                message: "no access for this origin: " + origin,
-                headers: dataHead
-              }
-            );
+            "src" */ //});
+    //https://stackoverflow.com/questions/35902490/requirejs-difference-between-global-require-and-module-require
+    //return require(["browserii.mjs"], async (m) => {
+    //This function will be called when all the dependencies
+    //listed above are loaded. Note that this function could
+    //be called before the page is loaded.
+    //This callback is optional.
+    const { locs, places, crs } = m.Browseri();
+    var iMCard = null,
+      mc = null;
+    const initializeMCard = () => {
+      if (!iMCard) {
+        console.log("initializing mastercard api");
+        mc = locs.MasterCardAPI;
+        iMCard = true;
+        mc.init({
+          sandbox: secrets.NODE_ENV !== "production",
+          authentication: new mc.OAuth(
+            secrets.MASTERCARD_CONSUMER_KEY,
+            Buffer.from(secrets.MASTERCARD_P12_BINARY, "base64"),
+            "keyalias",
+            "keystorepassword"
+          )
         });
       }
+    };
+    const mastercardRoute = async (req, func) => {
+      const cb = (error, data) => (error ? error : data);
+      initializeMCard();
+      let rs = null;
+      if (func === "getAtms") {
+        const {
+          PageLength, //"5"
+          PostalCode, //"11101"
+          PageOffset //"0"
+        } = req.body; //query
+        rs = await locs.ATMLocations.query(
+          {
+            PageLength,
+            PostalCode,
+            PageOffset
+          },
+          cb
+        );
+      } else if (func === "getMerchants") {
+        const { countryCode, latitude, longitude, distance } = req.body; //query
+        const q = {
+          pageOffset: 0,
+          pageLength: 10,
+          radiusSearch: "true",
+          unit: "km",
+          distance,
+          place: {
+            countryCode,
+            latitude,
+            longitude
+          }
+        };
+        rs = await places.MerchantPointOfInterest.create(q, cb);
+      } else if (func === "getNames") {
+        rs = await places.MerchantCategoryCodes.query({}, cb);
+      } else if (func === "getTypes") {
+        rs = await places.MerchantIndustries.query({}, cb);
+      }
+      return rs && rs;
+    };
+    const cors = crs({
+      origin: true,
+      allowedHeaders: [
+        "Access-Control-Allow-Origin",
+        "Access-Control-Allow-Methods",
+        "Content-Type",
+        "Origin",
+        "X-Requested-With",
+        "Accept"
+      ],
+      methods: ["POST", "OPTIONS"],
+      credentials: true
     });
+    //return async handleRequest(request)async (req) => {
+    var origin = req.get("Origin");
+    var allowedOrigins = ["https://vau.money", "https://jwi5k.csb.app"];
+    if (allowedOrigins.indexOf(origin) > -1) {
+      // Origin Allowed!!
+      if (req.method === "OPTIONS") {
+        // Method accepted for next request
+        return new Response(
+          {},
+          {
+            status: "200",
+            statusText: "successful header check for POST process: " + req.url,
+            headers: {
+              ...dataHead,
+              "Access-Control-Allow-Methods": "POST"
+            }
+          }
+        );
+      } else {
+        let rs = null;
+        if (req.url === "/deposit") {
+          rs = await mastercardRoute(req, "getAtms");
+        } else if (req.url === "/merchant_names") {
+          rs = await mastercardRoute(req, "getNames");
+        } else if (req.url === "/merchant_types") {
+          rs = await mastercardRoute(req, "getTypes");
+        } else if (req.url === "/merchants") {
+          rs = await mastercardRoute(req, "getMerchants");
+        }
+        if (rs) {
+          //isBase64Encoded: false,
+
+          return new Response(
+            {
+              data: rs
+            },
+            {
+              status: "200",
+              message: "success: " + req.url,
+              headers: dataHead
+            }
+          );
+        } else {
+          return new Response(
+            {},
+            {
+              status: "500",
+              message: "no success doof: " + req.url,
+              headers: dataHead
+            }
+          );
+        }
+      }
+    } else
+      return new Response(
+        {},
+        {
+          status: "400",
+          message: "no access for this origin: " + origin,
+          headers: dataHead
+        }
+      );
+    //});
+    //}
+    //});
   }
 }
 /*addEventListener('fetch', event => {
