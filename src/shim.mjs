45b0d797e6490352967dbe130445a06181d2bc2d
Nick Carducci <nmcarducci@gmail.com>
2021-11-23 21:55:22 -0500

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -17,9 +17,9 @@ async function noException(req, env) {
       return new Response(JSON.stringify(env));
   // key => Object ID
   //https://linc.sh/blog/durable-objects-in-production
-  const backbank = env.EXAMPLE_CLASS.idFromName(new URL(req.url).pathname);
+  const backbank = env.EXAMPLE_CLASS_DURABLE_OBJECT.idFromName(new URL(req.url).pathname);
   // boot instance, if necessary //https://<worker-name>.<your-namespace>.workers.dev/
-  const instance = await env.EXAMPLE_CLASS.get(backbank);
+  const instance = await env.EXAMPLE_CLASS_DURABLE_OBJECT.get(backbank);
   // Forward the current HTTP request to it
   return instance.fetch(req, env);
   //new Response({})
