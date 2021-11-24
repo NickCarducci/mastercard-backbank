6a8387577171ed039cef915b9222cf137ad15c05
Nick Carducci <nmcarducci@gmail.com>
2021-11-24 02:29:54 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 //importScripts('./require.js');
+import require from "./require.js";
 //https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.6/require.min.js
 export class DurableObjectExample {
   constructor(el, env) {
@@ -27,7 +28,6 @@ export class DurableObjectExample {
     };
     
     //return fetch(sentryUrl, { body: JSON.stringify(b), method: 'POST' })
-    const require = await fetch("./require.js");
     
     if(require){
       require.config({
