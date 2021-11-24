f19b66b8df1f39f973a377e0554a3734c611c390
Nick Carducci <nmcarducci@gmail.com>
2021-11-24 03:34:01 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -27,7 +27,8 @@ export class DurableObjectExample {
 
     //return fetch(sentryUrl, { body: JSON.stringify(b), method: 'POST' })
 
-    return import("./require.js").then(() => {
+    return import("./require.js").then(obj => {
+      const require = obj.require
       if (!require) {
         return new Response(
           {},
