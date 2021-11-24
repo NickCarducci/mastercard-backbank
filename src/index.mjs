0fb854f5612ca6e173bf3faa13da8b0db681f109
Nick Carducci <nmcarducci@gmail.com>
2021-11-24 03:57:53 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -27,7 +27,7 @@ export class DurableObjectExample {
 
     //return fetch(sentryUrl, { body: JSON.stringify(b), method: 'POST' })
     var require = null;
-    return import("./require.js").then(async obj => {
+    return await import("./require.js").then(async obj => {
       require = await obj.require
       if (!require) {
         return new Response(
