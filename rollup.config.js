2e92020fd9e1f767d92b99ba1b0fa5b25dc57eec
Nick Carducci <nmcarducci@gmail.com>
2021-11-25 22:29:56 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ import nodePolyfills from 'rollup-plugin-polyfill-node';
 //import globals from 'rollup-plugin-node-globals';
 //import builtins from 'rollup-plugin-node-builtins';
 //import webworkify from 'rollup-plugin-webworkify'
+import replace from 'rollup-plugin-replace';
 export default {
   inlineDynamicImports:true,
   input: "src/shim.mjs",
@@ -32,6 +33,13 @@ export default {
     commonjs({
       //include: ["node_modules/**"],
       //exclude: ["node_modules/process-es6/**"]
+    }),
+    /*babel({
+      exclude: 'node_modules/**',
+    }),*/
+    replace({
+      exclude: 'src/browserii.js',
+      //ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
     json(),
     terser(),
