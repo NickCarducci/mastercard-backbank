34299ac38d4fd93b29b563820051dbab1986a6df
Nick Carducci <nmcarducci@gmail.com>
2021-11-25 21:19:53 -0500

    Update webworkify.js

--- src/webworkify.js
+++ src/webworkify.js
@@ -1,2 +1,13 @@
-var work = require('webworkify');
-var w = work(require('./index.mjs'));
+
+const browserify = require("browserify");
+const work = require('webworkify');
+   
+browserify(/*{
+    standalone:"window"
+  }*/)
+    .add("./index.mjs")
+    .bundle(function (err) {
+      if (err) throw err;
+    })
+    .pipe(fs.createWriteStream("./indexi.js"));
+var w = work(require('./indexi.js'));
