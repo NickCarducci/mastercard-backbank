a39d4bf99cdcdd31820f7243cda56c7518e373ae
Nick Carducci <nmcarducci@gmail.com>
2021-11-27 21:40:44 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,16 @@
 //const work = require('webworkify');
 //import * as common from "./browseri.js"
 import Window from "./browserii.js"
+import { rollup, watch } from "rollup";
+import path from "path";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import { babel } from "@rollup/plugin-babel";
+import legacy from '@rollup/plugin-legacy';
+import replace from '@rollup/plugin-replace';
+import { generate } from 'astring'
 
 //const { locs, places, crs } = module.Window(); //Window.sourcesContent();
 export class DurableObjectExample {
@@ -13,6 +23,101 @@ export class DurableObjectExample {
       // After initialization, future reads do not need to access storage.
       this.value = stored || 0;
       //this.modules = work(this)??
+
+      const presets = [
+        [
+          "@babel/preset-env",
+          {
+            targets: 'defaults',
+            //"esmodules": true,
+            modules: "auto"
+          }
+        ],
+        //"@babel/preset-react"
+      ];
+
+      const plugins = [
+          replace({ 
+            include: './src/browseri.js',
+            values: {//https://stackoverflow.com/questions/40568580/rollup-js-how-import-a-js-file-not-es6-module-without-any-change-myvarextras
+              'module.exports =': 'export default'
+            }//like banner, footer
+          }),
+        nodeResolve({
+          browser: true,
+          only: [/^\.{0,2}\//],
+          extensions: [".js"],
+          mainFields: ["module", "main"]
+        }),
+        legacy({  'browserii.js': 'Window' }),
+        commonjs({
+          include: ["node_modules/**"],
+          exclude: ["node_modules/process-es6/**/*","notes/**/*","src/builders/**/*"]
+        }),
+        babel({
+          babelHelpers: "bundled",
+          presets,
+          exclude: "node_modules/**" // only transpile our source code
+        }),
+        json(),
+        terser()
+      ];
+      const inputOptions = {
+        external: ['cors', 'mastercard-locations','mastercard-places'],
+        input: "src/browseri.js",
+        plugins
+      };
+      const output = {
+        //banner,footer,
+        name: "Window",
+        strict: false,
+        file: "src/browserii.js",
+        format: "iife",
+        sourcemap: false,
+        //globals:{"Window":this.storage
+      };
+      const watchOptions = {
+        ...inputOptions,
+        output: [output],
+        watch: {
+          buildDelay: 5000,
+          chokidar: {},
+          clearScreen: true,
+          skipWrite: false,
+          exclude: ["node_modules/**/*","notes/**/*","src/builders/**/*"],
+          include: "src/**/*"
+        }
+      };
+      console.log("PLUGINS PASSED");
+      const watcher = watch(watchOptions);
+      console.log("WATCHER INITIALIZED");
+      watcher.on("event", (event) => {
+        //event.result.cache.modules.ast.sourceType = "module"
+        const ast = event.result.cache.modules.ast//.body
+        
+        const esm = generate(ast)
+      this.el.storage.put("umd", JSON.stringify(esm)) 
+
+        console.log("G-FORCE:rollup: ", JSON.stringify(event));
+        if (event.code === "BUNDLE_START") 
+        } else if (event.code === "START") 
+          //   START        — the watcher is (re)starting
+        } else if (event.code === "END") {
+          watcher.close();
+        } else if (event.code === "ERROR") {
+        } else if (event.code === "BUNDLE_END") {
+        }
+        if (event.result) {
+          event.result.close();
+        }
+      });
+
+      rollup(inputOptions)
+        .then(async (bundle) => {
+          await bundle.write(output);
+        })
+        .catch((err) => console.log("rollup.rollup error", err.message));
+
       
       //this.el.storage.delete("umd");
       //const worker = work(import("./browseri.js")) 
