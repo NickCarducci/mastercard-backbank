a326fa21207d80e211002bdf58ce585e19471ad6
Nick Carducci <nmcarducci@gmail.com>
2021-12-05 22:37:39 -0500

    Update rollupCommon.mjs

--- src/builders/rollupCommon.mjs
+++ src/builders/rollupCommon.mjs
@@ -1,6 +1,6 @@
 import * as locs from "mastercard-locations";
 import * as places from "mastercard-places";
-import * as crs from "cors");
+import * as crs from "cors";
 import { rollup, watch } from "rollup";
 import path from "path";
 import commonjs from "@rollup/plugin-commonjs";
