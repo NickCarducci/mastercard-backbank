88a4de9a1178214533a2e6e8210d2b66b80e8906
Nick Carducci <nmcarducci@gmail.com>
2022-01-08 21:51:04 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -3,7 +3,7 @@
 import fs from "fs";
 import crypto from "crypto";
 import path from "path";
-import ast from "abstract-syntax-tree";
+import * as ast from "abstract-syntax-tree";
 /*import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);*/
