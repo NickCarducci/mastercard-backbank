b8fb6469a63f1a2d178c9c4e02804e8c89b30e76
Nick Carducci <nmcarducci@gmail.com>
2022-02-15 09:14:03 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,16 @@
 import { terser } from 'rollup-plugin-terser'
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
-//import cjs from "rollup-plugin-cjs-es";
-//nodePolyfills() => -assert node_modules/resolve/lib/core.json (2:9)
-/*1: {
-2:   "assert": true,
-             ^
-3:   "assert/strict": ">= 15",
-  +builtin  node_modules/globals/globals.json (2:10)
-15
-1: {
-16
-2:   "builtin": {*/
-
-//import nodePolyfills from 'rollup-plugin-polyfill-node';
-//import legacy from '@rollup/plugin-legacy';
 import { dependencies } from './package.json';
 
 const exportType = moduleId=>{
-  
-  
   return {//export as sourceMap<Object> or Promise?
     "src/index.mjs": "function"
   }
-    
 }
 
 export default {
-  //inlineDynamicImports:true,
   input: "src/shim.mjs",
-    //external: Object.keys(dependencies),
-  manualChunks: {
-    'vendor': ['mastercard-locations', 'mastercard-places', 'cors']
-  },
   output: {
     exports: 'named',
     //name: "app",
@@ -42,12 +20,20 @@ export default {
     sourcemap: true
   },
   plugins: [
-    /*cjs({exportType,
-      nested: true
-    }),*/
-  //legacy({  'browserii.js': 'Window' }),
-    commonjs({transformMixedEsModules:true,include: 'node_modules/**'}),
-    //nodePolyfills(),
     nodeResolve({browser: true,}),
-    terser()]
+    terser()
+  ]
 };
+
+/*
+manualChunks: {
+  'vendor': ['mastercard-locations', 'mastercard-places', 'cors']
+},
+inlineDynamicImports: true,
+external: Object.keys(dependencies),
+plugins: [cjs({exportType,nested: true}),
+  nodePolyfills(),
+  commonjs({transformMixedEsModules:true,include: 'node_modules/**'}),
+  legacy({  'browserii.js': 'Window' }),
+]
+*/
