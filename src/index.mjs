bc4bfea94f17b69b554ab32f044871f3f3d2ce9e
Nick Carducci <nmcarducci@gmail.com>
2022-02-16 15:20:11 -0500

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import Window from "./dependencies/shim.js";
+import Window from "./dependencies/shim.js";//"./dependencies/commonUMD.js"
 
 export class DurableObjectExample {
   constructor(el, env) {
