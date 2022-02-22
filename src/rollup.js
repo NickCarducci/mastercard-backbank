a112fac0983fb0934992664a163d195df7f785c2
Nick Carducci <nmcarducci@gmail.com>
2022-02-21 19:40:40 -0500

    Update rollup.js

--- src/rollup.js
+++ src/rollup.js
@@ -1,39 +1,27 @@
 //rollupCommonToUMD.js
 import { rollup, watch } from "rollup";
-//import json from '@rollup/plugin-json';
-//import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import nodePoly from 'rollup-plugin-polyfill-node';
+import nodePoly from "rollup-plugin-polyfill-node";
 
-const output = {
-  banner:"const app = () => ",
-  footer:"export default app",
-  //name: "Window",//umd only - no default
-  strict: false,
-  file: "src/built.js",
-  format: "iife", //"umd",//cannot call a namespace/no default, "Window"//"es",
-  sourcemap: false
-  //globals:{"Window":this.storage}
-};
-
-const plugins = [
-  //nodeResolve({ preferBuiltins: false, jsnext: true }),
-  //json(),
-  nodePoly(),
-  nodeResolve(),
-  /*commonjs({
-    //include: 'node_modules/**', // Default: undefined
-  }),*/
+const pages = [
+  {
+    //name:"Window",
+    format: "iife",
+    sourcemap: false,
+    strict: false,
+    banner: "const app = () => ",
+    file: "src/built.js",
+    footer: "export default app"
+  }
 ];
-
-const inputOptions = {
-  //external: ['cors', 'mastercard-locations','mastercard-places'],
-  input: "src/dependencies/shim.mjs", //[crs,places,locs],
-  plugins
+const manifest = {
+  input: "src/dependencies/shim.mjs",
+  plugins: [nodePoly(), nodeResolve()]
 };
-const watchOptions = {
-  ...inputOptions,
-  output: [output],
+
+const watchable = {
+  ...manifest,
+  output: pages,
   watch: {
     buildDelay: 5000,
     chokidar: {},
@@ -60,15 +48,11 @@ const watchOptions = {
     }
   },
   inlineDynamicImports: true
-  //external: Object.keys(dependencies),
-  /*manualChunks: {
-    'vendor': ['mastercard-locations', 'mastercard-places', 'cors']
-  },*/
 };
 console.log("PLUGINS PASSED");
-const watcher = watch(watchOptions);
+const watcher = watch(watchable);
 console.log("WATCHER INITIALIZED");
-watcher.on("event", (event) => { 
+watcher.on("event", (event) => {
   if (event.code === "BUNDLE_START") {
   } else if (event.code === "START") {
   } else if (event.code === "END") {
@@ -77,6 +61,69 @@ watcher.on("event", (event) => {
   } else if (event.code === "BUNDLE_END") {
   }
   if (event.result) {
+    const ast = event.result.cache.modules[0].ast; //.body
+    console.log(ast, " is Abstract Syntax Tree of dependencies");
+    event.result.close();
+  }
+});
+
+rollup(manifest)
+  .then(async (bundle) => {
+    console.log(Object.keys(bundle), " is bundle");
+    return pages.forEach(async (output) => await bundle.write(output));
+  })
+  .catch((err) => console.log("rollup.rollup error", err.message));
+
+/*
+//import json from '@rollup/plugin-json';
+//import commonjs from "@rollup/plugin-commonjs";
+const output = {
+  banner:"const app = () => ",
+  footer:"export default app",
+  strict: false,
+  file: "src/built.js",
+  format: "iife", //"umd",//cannot call a namespace/no default, "Window"//"es",
+  sourcemap: false
+  //name: "Window",//umd only - no default
+  //globals:{"Window":this.storage}
+};
+
+const plugins = [
+  //nodeResolve({ preferBuiltins: false, jsnext: true }),
+  //json(),
+  nodePoly(),
+  nodeResolve(),
+  commonjs({
+    //include: 'node_modules/**', // Default: undefined
+  })
+];
+const inputOptions = {
+  //external: ['cors', 'mastercard-locations','mastercard-places'],
+  input: "src/dependencies/shim.mjs", //[crs,places,locs],
+  plugins
+};
+const watchable = {
+  ...inputOptions,
+  output: [output],
+  watch: {
+    buildDelay: 5000,
+    chokidar: {},
+    clearScreen: true,
+    skipWrite: false,
+    exclude: ["node_modules/** /*", "notes/** /*", "src/builders/** /*"],
+    include: "src/** /*"
+  },
+  onwarn:()=>{},
+  inlineDynamicImports: true
+  //external: Object.keys(dependencies),
+  manualChunks: {
+    'vendor': ['mastercard-locations', 'mastercard-places', 'cors']
+  },
+};
+const watcher = watch(watchable);
+console.log("WATCHER INITIALIZED");
+watcher.on("event", (event) => { 
+  if (event.result) {
     //console.log(event.result.cache.modules[0], " is event.result.cache.modules[0]");
     const ast = event.result.cache.modules[0].ast; //.body
     console.log(ast, " is Abstract Syntax Tree of dependencies");
@@ -84,11 +131,4 @@ watcher.on("event", (event) => {
     //this.el.storage.put("esm", JSON.stringify(esm))
     event.result.close();
   }
-});
-
-rollup(inputOptions)
-  .then(async (bundle) => {
-    console.log(Object.keys(bundle), " is bundle");
-    await bundle.write(output);
-  })
-  .catch((err) => console.log("rollup.rollup error", err.message));
+*/
