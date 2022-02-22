75b5f80d3eda2a6f820254832536896fcff5bba4
Nick Carducci <nmcarducci@gmail.com>
2022-02-21 19:05:44 -0500

    Update rollup.js

--- src/rollup.js
+++ src/rollup.js
@@ -5,12 +5,12 @@ import nodePoly from "rollup-plugin-polyfill-node";
 
 const pages = [
   {
+    banner: "const app = () => ",
+    footer: "export default app",
+    file: "src/built.js",
     format: "iife",
     sourcemap: false,
     strict: false,
-    banner: "const app = () => ",
-    file: "src/built.js",
-    footer: "export default app"
   }
 ];
 const manifest = {
