1468c3e513c95966ac4b941c13974f921d120938
Nick Carducci <nmcarducci@gmail.com>
2022-02-21 19:44:13 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,14 @@
 import { terser } from 'rollup-plugin-terser'
-//import nodeResolve from "@rollup/plugin-node-resolve";
-import { dependencies } from './package.json';
-
-const exportType = moduleId=>{
-  return {//export as sourceMap<Object> or Promise?
-    "src/index.mjs": "function"
-  }
-}
-
 export default {
   input: "src/shim.mjs",
   output: {
     exports: 'named',
-    //name: "app",
     strict: false,
     file: "dist/shim.mjs",
     format: "es",
     sourcemap: true
   },
   plugins: [
-    //nodeResolve({browser: true,}),
     terser()
   ]
 };
