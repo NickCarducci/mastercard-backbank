ee1ad8ad594f11134e0bd136794e438d4f13a438
Nick Carducci <nmcarducci@gmail.com>
2022-03-01 19:39:05 -0500

    Update shim.mjs

--- src/dependencies/shim.mjs
+++ src/dependencies/shim.mjs
@@ -5,13 +5,13 @@ import * as crs from "cors";*/
 //https://github.com/nodejs/node/blob/08be585712774904bccbf4a43e481895a641464f/doc/api/modules.md#exports-shortcut
 export default function require(app/* ... */) {
   var module = { exports: {} };//const is shallow?
-  ((module, exports) => {
+  ((module, exports, app) => {
     /*function app() {
       locs,places,crs
     }*/
     exports = app;//exports != module.exports && export an empty default object (const, page)
     module.exports = app;//export app != default object (const, page)
-  })(module, module.exports);
+  })(module, module.exports, app);
   return module.exports;
 }
 //https://stackoverflow.com/a/52351150/11711280
