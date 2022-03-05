ff8b2debc212a9a7f273bf8e7908fd1987b20e99
Nick Carducci <nmcarducci@gmail.com>
2022-03-04 19:46:46 -0500

    Update require.js

--- src/require.js
+++ src/require.js
@@ -2296,4 +2296,5 @@ require = (function (global, setTimeout) {
   //Set up with config info.
   req(cfg);
 })(require, typeof setTimeout === "undefined" ? undefined : setTimeout);
-export default function Required () {return{require,define}};
+function Required () {return{require,define}};
+export {Required as default}
