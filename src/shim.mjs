6bfcef2f4ec6ffd1f5dff9de13bde2d1d8d88006
Nick Carducci <nmcarducci@gmail.com>
2022-04-13 12:06:59 -0400

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -1,7 +1,7 @@
 //exports, and imports, at the same time!
 import * as R from "./index.mjs";
-const R = { ...R }
-export { DurableObjectExample, Require } = R
+const R = { ...R };
+export { DurableObjectExample, Require } = R;
 //export { Require } from "./require.mjs";
 
 export default {
