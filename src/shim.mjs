3b3e90175fb60dce6c00df1c15f6e0afb2247978
Nick Carducci <nmcarducci@gmail.com>
2022-04-13 13:55:08 -0400

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -28,10 +28,24 @@ async function noException(req, env) {
   // Forward the current HTTP request to it
   const resp = await instance.fetch(req, env);
   const r = await resp.json();
-    return new Response(`{
+    /*return new Response(`{
         ok: true,
         data: ${r}
-    }`);
+    }`);*/
+  const dataHead = {
+    "Content-Type": "application/json"
+  };
+  return new Response(
+    {
+        ok: true,
+        data: r.data
+    },
+    {
+      status: "200",
+      message: "success: " + req.url,
+      headers: dataHead
+    }
+  );
   
   //new Response({})
 }
