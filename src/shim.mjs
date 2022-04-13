00301b0a2bbb9d12cb8ab621837b4eb6e5411a0e
Nick Carducci <nmcarducci@gmail.com>
2022-04-13 18:57:53 -0400

    Update shim.mjs

--- src/shim.mjs
+++ src/shim.mjs
@@ -35,7 +35,7 @@ async function noException(req, env) {
     const instance = env.REQUIRE_CLASS_DURABLE_OBJECT.get(backbank);
     const resp = instance && (await instance.fetch(req, env));
     const require = resp && (await resp.json());
-    return new Promise((resolve) => require && resolve(require));
+    return new Response((resolve) => require && resolve(require));
   };
 
   const require = await makeRequire(req, env);
