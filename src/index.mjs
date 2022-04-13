8d5705c79f81813e629b9bb33e24b923f4d2a99e
Nick Carducci <nmcarducci@gmail.com>
2022-04-13 17:17:45 -0400

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -35,7 +35,7 @@ export class DurableObjectExample {
         const instance = env.REQUIRE_CLASS_DURABLE_OBJECT.get(backbank);
         const resp = await instance.fetch(req, env);
         const require = resp && await resp.json();
-        return require && new Promise(resolve=>resolve(require));
+        return new Promise(resolve=>require && resolve(require));
       }
       //fn.apply(this, [locs,places,crs])
       //this.value = {locs,places,crs}//Window;
