8436d171755de2616b55ac79af1a3fa3a9fab47a
Nick Carducci <nmcarducci@gmail.com>
2022-04-14 05:32:29 -0400

    Update require.mjs

--- src/require.mjs
+++ src/require.mjs
@@ -22,7 +22,8 @@ export class Require {
     this.el.blockConcurrencyWhile(async () => {
       //let stored = await this.el.storage.get("esm"); //Read requests	100,000 / day, ($free)
       //this.value = stored || 0;
-      var b;
+      var b = () => {};
+      b();
 
       var setTimeout,
         T = (x) => typeof x;
