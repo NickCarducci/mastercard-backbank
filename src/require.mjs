2bd86256b395d66b4d1118bab34dec19e6770373
Nick Carducci <nmcarducci@gmail.com>
2022-04-16 18:42:44 -0400

    Update require.mjs

--- src/require.mjs
+++ src/require.mjs
@@ -1482,7 +1482,7 @@ export class Require {
 
     //this named by onload event, for anonymous modules or without context; IE 6-8 anonymous define() call, requires interactive document.getElementsByTagName("script")
     //...[ 'dataMain','baseElement', 'mainScript', 'subPath', 'src', 'head', 'dependency'].reduce((x,next)=>x[next]=null),
-    console.log("build product (of Require) :", build);
+    console.log("build product (of Require) :", JSON.stringify(build));
     const state = {
       //This...
       //The 'rest parameter:' spread a fat arrow's args for function arguments
