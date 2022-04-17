d9c7b63432d886c72d8c0aa0f8d65c5e2212e04b
Nick Carducci <nmcarducci@gmail.com>
2022-04-16 20:27:05 -0400

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -73,7 +73,7 @@ export class DurableObjectExample {
       //const requirer = this.makeRequire(req);
       //console.log("requirer: ", requirer);
       return await this.makeRequire(req) //new Promise((resolve) => requirer && resolve(requirer)) // this.makeRequire(req)
-        .then(async (r) => await r.json())
+        //.then(async (r) => await r.json())
         .then(async (requirer) => {
           console.log("requirer", JSON.stringify(requirer));
           const locs = requirer("mastercard-locations");
