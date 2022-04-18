a7d19518fbb3670b71b52da85bdd2bba7f5aa66a
Nick Carducci <nmcarducci@gmail.com>
2022-04-18 11:33:21 -0400

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -6,28 +6,10 @@ export class DurableObjectExample {
       JSON.stringify(env)
     ); //el.textContent
     this.handle = async (requi, req) => {
-      const requir = requi
-        .read()
-        .then(async function processText({ done, value }) {
-          var result = "",
-            charsReceived = 0;
-          // done = true, if the stream has already given you all its data.
-          // value = some_data. Always undefined when done is true.
-          if (done) {
-            console.log("Stream complete : ", result);
-            const product = String.fromCharCode.apply(
-              null,
-              Array(result) /*Uint8Array*/
-            );
-            console.log("Stream complete : ", product);
-            return product;
-          }
-          charsReceived += value.length; // 'value' for fetch streams is a Uint8Array
-          const chunk = value;
-          console.log(`Total (${charsReceived}) Uint8Array = (${chunk})++`);
-          result += chunk;
-          return await requi.read().then(processText); // Read some more, and call this function again
-        });
+      var reader = new FileReader(),
+        requir;
+      reader.readAsDataURL(requi);
+      reader.onloadend = async () => (requir = reader.result);
       /*console.log(
         "gotten/(-piped) REQUIRE_CLASS_DURABLE_OBJECT (requirer) :",
         requir
