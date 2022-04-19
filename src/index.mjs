85b32e4b2d9932e001f4157714b3a97a3afb1f8d
Nick Carducci <nmcarducci@gmail.com>
2022-04-19 12:37:16 -0400

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -343,7 +343,7 @@ class Require {
         NAME = cfg && cfg.context ? cfg.context : NAME;
         ctx = e_(ctxs).yes(NAME) && ctxs[NAME];
         console.log(ctx, ctx.require);
-        ctx = ctx ? ctx : (ctxs[NAME] = build.start.newRequireable(NAME)); //dependency
+        ctx = ctx ? ctx : (ctxs[NAME] = () => build.start.newRequireable(NAME)); //dependency
         console.log(ctx, ctx.require);
         cfg && ctx.configure(cfg);
         return ctx.require(ds, cb, eb);
