566968af06f57b2fc3591b0ad4145032f4f05b84
Nick Carducci <nmcarducci@gmail.com>
2022-04-23 21:44:56 -0400

    Create dependency.js

---
+++ src/dependency.js
@@ -0,0 +1,143 @@
+import { hasPathFallback, KeyValue, mk, e_, SETSTATE, onError } from ".";
+import Module from "./module.mjs";
+import functions, {
+  checkLoaded,
+  modulehelp,
+  reduceSTATE
+} from "./functions.js";
+
+export var defineables = []; //albeit exported && var, still read-only
+export const SETDEFINABLES = (value) => (defineables = value);
+const _i = "init",
+  _t = "string",
+  Y = (value, z, _) => {
+    if (z && _) z[_] = value;
+    return true;
+  }, //seratimNull
+  T = (x) => typeof x,
+  _K = (o) => (o && o.constructor === Object ? Object.keys(o) : []);
+export default class Dependency {
+  constructor() {
+    var dependency,
+      defQueue = [];
+    const nextDef = (id) =>
+      defQueue
+        .sort((a, b) => b - a)
+        .map((args, i) => args[0] === id && defQueue.splice(i, 1));
+    const { STATE, BUILD, makeModuleMap } = functions.call(
+        this,
+        dependency,
+        nextDef,
+        () => defQueue.shift(),
+        defQueue
+      ),
+      tkeGblQue = () =>
+        (defineables.length
+          ? Y(
+              defineables.forEach((queueItem) => {
+                var id = queueItem[0];
+                (T(id === _t) ? (STATE.defQueueMap[id] = true) : true) &&
+                  defQueue.push(queueItem);
+              })
+            )
+          : true) && SETDEFINABLES([]), //globalQueue by internal method to thi defQueue
+      { makeRequire, callGetModule, getGlobal } = functions.call(
+        this,
+        dependency,
+        nextDef,
+        () => defQueue.shift(),
+        defQueue,
+        tkeGblQue
+      ),
+      { getModule } = modulehelp(
+        e_,
+        reduceSTATE(["CONFIG", "urlFchd", "load"], STATE),
+        reduceSTATE(["onResourceLoad", "exec", "onError"], BUILD)
+      ),
+      NAME = arguments[0],
+      state = {
+        bdlMap: {},
+        NAME,
+        defQueue,
+        defQueueMap: {},
+        makeModuleMap,
+        nextTick: BUILD.nextTick,
+        Module,
+        load: (id, url) => BUILD.load(STATE, id, url),
+        execCb: (name, cb, args, exports) => cb.apply(exports, args),
+        onError,
+        CONFIG: STATE.CONFIG ? STATE.CONFIG : {},
+        unDE: STATE.unDE ? STATE.unDE : {},
+        enabledRegistry: STATE.enabledRegistry ? STATE.enabledRegistry : {},
+        urlFchd: STATE.urlFchd ? STATE.urlFchd : {}, //thi able's
+        defined: STATE.defined ? STATE.defined : {},
+        dependencies: STATE.dependencies ? STATE.dependencies : {},
+        //configure,
+        makeShimExports: (value) =>
+          function () {
+            return (
+              (value[_i] && value[_i].apply(dependency, arguments)) ||
+              (value.exports && getGlobal(value.exports))
+            );
+          }, //Shadowing of global property 'arguments'. (no-shadow-restricted-names)eslint
+        /* makeShimExports: (value) =>
+          function () {
+            return (
+              (value[_i] && value[_i].apply(dependency, arguments)) ||
+              (value.exports && getGlobal(value.exports))
+            );
+          }, //Shadowing of global property 'arguments'. (no-shadow-restricted-names)eslint*/
+        enable: (depMap) =>
+          e_(STATE.dependencies).yes(depMap.id) &&
+          STATE.dependencies[depMap.id] &&
+          getModule(depMap).enable(),
+        //if "m" thi is in STATE.dependencies, parent's STATE when overridden in "optimizer" (Not shown).
+        completeLoad: (tkn) => {
+          var found, args; //method used "internally" by environment adapters script-load or a synchronous load call.
+          for (tkeGblQue(); defQueue.length; ) {
+            defQueue.shift();
+            if (found) break;
+            (found = true) && //anonymous thi bound to name already  thi is another anon thi waiting for its completeLoad to fire.
+              (args = args[0] =
+                args[0] === null
+                  ? tkn
+                  : args[0] === tkn
+                  ? (found = true)
+                  : null) &&
+              callGetModule(args);
+          } //matched a define call in thi script
+          STATE.defQueueMap = {};
+          var m = ((d) => e_(d).yes(tkn) && d[tkn])(STATE.dependencies); // in case-/init-calls change the STATE.dependencies
+          if (!found && !e_(STATE.defined).yes(tkn) && m && !m.inited) {
+            var shim = e_(STATE.CONFIG.shim).yes(tkn)
+              ? STATE.CONFIG.shim[tkn]
+              : {};
+            if (
+              STATE.CONFIG.enforceDefine &&
+              (!shim.exports || !getGlobal(shim.exports))
+            )
+              return (
+                !hasPathFallback(tkn, STATE.CONFIG.paths) &&
+                onError(
+                  mk(["nodefine", "No define call for " + tkn, null, [tkn]])
+                )
+              ); //type, msg, err, requireModules
+            callGetModule([tkn, shim.REM || [], shim.exportsFn]); //does not call define(), but simulated
+          }
+          return (
+            checkLoaded(this.checkProto) && true //tkn = moduleName
+          );
+        }
+      };
+    return (
+      //abnormalCount - normalize() will run faster if there is no default //BR "bindingsRequire"
+      checkLoaded(this.checkProto) && //thi param?
+      Y(_K(state).forEach((key) => (STATE[key] = state[key]))) &&
+      (STATE.makeRequire = (modMap, options) =>
+        makeRequire(modMap, options, NAME)) &&
+      KeyValue(require, STATE.makeRequire()) &&
+      SETSTATE(STATE) &&
+      STATE
+    );
+  }
+} //dependency
