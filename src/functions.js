2deae866de13af94ffe0a733821f65ab0c48265e
Nick Carducci <nmcarducci@gmail.com>
2022-04-23 21:44:27 -0400

    Create functions.js

---
+++ src/functions.js
@@ -0,0 +1,542 @@
+import {
+  hasPathFallback,
+  KeyValue,
+  mk,
+  e_,
+  dr,
+  tryCatch,
+  handlers,
+  normalize,
+  onError,
+  iifeapp,
+  isBrowser
+} from ".";
+export const reduceSTATE = (arr, STATE) => {
+  var newobject = {};
+  Object.keys(STATE).forEach(
+    (key) => arr.includes(key) && (newobject[key] = STATE[key])
+  );
+  return newobject;
+};
+export function nameToUrl() {
+  const { CONFIG, bdlMap } = this;
+  //token, ext, skipExt, pkgMain
+  var ext = arguments[1],
+    skipExt = arguments[2],
+    pkgMain =
+      e_(CONFIG.bundle).yes(arguments[0]) && CONFIG.bundle[arguments[0]], //already-normalized-tkn as URL. Use toUrl for the public API.
+    tkn = pkgMain ? pkgMain : arguments[0], //If slash or colon-protocol fileURLs contains "?" or even ends with ".js",
+    id = e_(bdlMap).yes(tkn) && bdlMap[tkn]; //assume use of an url, not a thi id.
+  id && nameToUrl(id, ext, skipExt); //filter out STATE.dependencies that are already paths.
+  const geturl = (url = "") => {
+      //Just a plain path, not thi name lookup, so just return it.
+      if (/^[/:?.]|(.js)$/.test(tkn)) return (url = tkn + (ext || "")); //Add extension if it is included. This is a bit wonky, only non-.js things pass
+      var paths = CONFIG.paths,
+        syms = tkn.split("/"); //an extension, thi method probably needs to be reworked. A thi that needs to be converted to a path.
+      for (let i = syms.length; i > 0; i -= 1) {
+        var pM = syms.slice(0, i).join("/"), //per thi name segment if path registered, start name, and work up
+          pP = e_(paths).yes(pM) && paths[pM]; //parentModule
+        pP && (pP = e_(pP).a() ? pP[0] : pP) && syms.splice(0, i, pP);
+        if (pP) break; //arr means a few choices; parentPath
+      }
+      (url = syms.join("/")) && //Join the path parts together, then figure out if baseUrl is needed.
+        (url += ext || (/^data:|^blob:|\?/.test(url) || skipExt ? "" : ".js")); ///^data\:|^blob\:|\?/
+
+      // prettier-ignore
+      return (url.charAt(0) === "/" || url.match(/^[\w+.-]+:/) ? "" : CONFIG.baseUrl) + url; ///^[\w\+\.\-]+:/
+    }, //Delegates to BUILD.load. Broken out as a separate function to
+    u = geturl;
+  return `${
+    CONFIG.urlArgs && !/^blob:/.test(u) ? u + CONFIG.urlArgs(tkn, u) : u
+  }`;
+} // If package-name, package 'main,' roots
+
+const Y = (value, z, _) => {
+  if (z && _) z[_] = value;
+  return true;
+}; //seratimNull
+export const modulehelp = (e_, STATE, BUILD, moduleProto) => {
+  const clrRegstr = (id) =>
+      KeyValue(`dependencies.${id}`, null, "delete") &&
+      KeyValue(`enabledRegistry.${id}`, null, "delete"),
+    depMap = (a0) => {
+      return {
+        dm: a0,
+        m: e_(STATE.dependencies).yes(a0.id) && STATE.dependencies[a0.id]
+      };
+    };
+  return {
+    getModule: ({ m, dm } = depMap) => {
+      const {
+          CONFIG: config = (CONFIG) => CONFIG.config,
+          urlFchd,
+          load
+        } = STATE,
+        manage = (map, ignore, id, depMaps) =>
+          map.yesdef && !ignore
+            ? new Promise((resolve) =>
+                KeyValue(`defined.${id}`, module.exports && resolve(""))
+              ).then(
+                () =>
+                  BUILD.onResourceLoad &&
+                  BUILD.onResourceLoad(
+                    STATE,
+                    map,
+                    depMaps.map((depMap) => depMap.normalizedMap || depMap)
+                  )
+              )
+            : true,
+        mold = (id, tkn) =>
+          e_(config).yes(id)
+            ? KeyValue(`CONFIG.config.${tkn}`, config[id])
+            : true,
+        loadd = () =>
+          !urlFchd[dm.url]
+            ? KeyValue(`urlFchd.${dm.url}`, true) && load(dm.id, dm.url)
+            : console.log(
+                `redundant STATE.load(${dm.id}, ${dm.url}) call (?), STATE.urlFchd[${dm.url}] === true`
+              ),
+        execute = (text, id) => {
+          const erro = tryCatch(BUILD.exec, [text]);
+          if (erro)
+            return onError(
+              mk([
+                "fromtexteval",
+                `fromText eval for ${id} failed: ${erro}`,
+                erro,
+                [id]
+              ])
+            );
+        };
+      const matches = [
+        "unDE",
+        "CONFIG",
+        "dependencies",
+        "makeRequire",
+        "bdlMap",
+        "completeLoad",
+        "enable",
+        "execCb",
+        "defined",
+        "defQueueMap"
+      ];
+      const { makeModuleMap, useInteractive, _e } = moduleProto;
+      return m
+        ? m
+        : KeyValue(
+            `dependencies.${dm.id}`,
+            new STATE.Module(
+              dm,
+              makeModuleMap,
+              useInteractive,
+              _e,
+              clrRegstr,
+              nameToUrl,
+              modulehelp(e_, STATE, BUILD, moduleProto).getModule,
+              onError,
+              ...reduceSTATE(matches, STATE),
+              manage,
+              mold,
+              loadd,
+              KeyValue(`startTime`, new Date().getTime()),
+              KeyValue(`enabledRegistry.${dm.id}`, this),
+              normalize,
+              BUILD.onError !== ((err) => err),
+              execute,
+              depMap
+            )
+          );
+    }
+  };
+};
+//mostly makerequire
+const _i = "init",
+  _t = "string",
+  Fn = "[object Function]",
+  T = (x) => typeof x;
+export default function (
+  dependency = arguments[0],
+  nextDef = arguments[1],
+  shiftDef = arguments[2],
+  defQueue = arguments[3],
+  tkeGblQue = arguments[4]
+) {
+  const { STATE, BUILD, makeModuleMap, e_, checkProto, moduleProto } = this; //Called from Dependency
+
+  const { getModule, clrRegstr } = modulehelp(
+      e_,
+      reduceSTATE(["CONFIG", "urlFchd", "load"], STATE),
+      reduceSTATE(["onResourceLoad", "exec", "onError"], BUILD),
+      moduleProto
+    ),
+    callGetModule = (args) =>
+      !e_(STATE.defined).yes(args[0]) &&
+      getModule(makeModuleMap(args[0], null, true))[_i](args[1], args[2]);
+  return {
+    callGetModule,
+    getGlobal: (value) =>
+      !value
+        ? value //dot-notation dependency
+        : value.split(".").reduce((previous, key) => dependency[previous], {}),
+    makeRequire: (modMap, o = (options) => options || {}, NAME) => {
+      const tool = (modMap, o, NAME) => {
+          return {
+            errr: (
+              //dependencies, callback, errorback
+              rem,
+              cb = (cb) =>
+                Y(
+                  o.enableBuildCallback &&
+                    cb &&
+                    e_(cb).string() === Fn &&
+                    (cb.__requireJsBuild = true)
+                ) && cb,
+              eb
+            ) =>
+              T(rem !== _t)
+                ? null
+                : e_(cb).string() === Fn
+                ? onError(
+                    mk([
+                      "requireargs",
+                      "Invalid ([object Function], -class?) require callback"
+                    ]),
+                    eb
+                  ) //Invalid call; id, msg, err, requireModule
+                : modMap && e_(handlers).yes(rem)
+                ? handlers[rem](
+                    STATE.dependencies[modMap.id],
+                    STATE.CONFIG.config,
+                    STATE.makeRequire,
+                    STATE.defined
+                  ) //when require|exports|module are requested && while thi is being STATE.defined
+                : BUILD.get
+                ? BUILD.get(STATE, rem, modMap, tool.parser)
+                : () => {
+                    var id, map;
+                    return (
+                      (map = makeModuleMap(rem, modMap, false, true)) &&
+                      (id = map.id) && //Normalize thi name from . or ..
+                      (!e_(STATE.defined).yes(id)
+                        ? onError(
+                            mk([
+                              "notloaded",
+                              `Module name ${id} has not been loaded yet for commonjs Dependencies' build : ` +
+                                NAME +
+                                !modMap && "; (No modMap) Use require([])"
+                            ])
+                          )
+                        : STATE.defined[id])
+                    );
+                  },
+            parser: (...args) => {
+              //function localRequire (dependencies, callback, errorback){}
+              if (tool.errr(...args)) return null;
+              const rem = args[0],
+                cb = args[1],
+                eb = args[2];
+
+              var requireMod;
+              const intakeDefines = () => {
+                for (tkeGblQue(); defQueue.length; ) {
+                  const args = shiftDef[0];
+                  if (null === args)
+                    return onError(
+                      mk([
+                        "mismatch",
+                        `Mismatched anonymous define() thi: ${
+                          args[args.length - 1]
+                        }`
+                      ])
+                    );
+
+                  callGetModule(args);
+                }
+                return (STATE.defQueueMap = {}) && true;
+              }; //"intake modules" //type, msg, err, requireModules //...id, REM, factory; "normalized by define()"
+              intakeDefines(); //Grab defines waiting in the dependency queue.
+              STATE.nextTick(
+                () =>
+                  intakeDefines() && //Mark all the STATE.dependencies as needing to be loaded.
+                  (requireMod = getModule(makeModuleMap(null, modMap))) && //collect defines that could have been added since the 'require call'
+                  (requireMod.skipMap = o.skipMap) && //store if 'map STATE.CONFIG' applied to thi 'require call' for STATE.dependencies
+                  requireMod[_i](rem, cb, eb, { enabled: true }) &&
+                  checkLoaded.call(checkProto)
+              );
+              return tool.parser;
+            }
+          };
+        },
+        namer = {
+          isBrowser,
+          defined: (id) =>
+            e_(STATE.defined).yes(makeModuleMap(id, modMap, false, true).id),
+          specified: (id = (id) => makeModuleMap(id, modMap, false, true).id) =>
+            e_(STATE.defined).yes(id) || e_(STATE.dependencies).yes(id),
+          toUrl: (
+            { i, isAlias, mNPE } = (mNPE) => {
+              const seg = mNPE.split("/")[0];
+              return {
+                i: mNPE.lastIndexOf("."),
+                isAlias: i !== -1 && (![".", ".."].includes(seg) || i > 1),
+                mNPE
+              };
+            }
+          ) => {
+            //moduleNamePlusExt
+            //URL path = thi name + .extension; requires 'thi name,' not 'plain URLs' like nameToUrl
+
+            const ext = isAlias ? mNPE.substring(i, mNPE.length) : null;
+            mNPE = isAlias ? mNPE.substring(0, i) : mNPE;
+            //file extension alias, not 'relative path dots'
+
+            const { nodeIdCompat, system, bundle } = STATE.CONFIG;
+            //also, "map" for outward facing code...//also, "packages" ""
+            const ar = [
+              mNPE,
+              modMap && modMap.id,
+              true,
+              nodeIdCompat,
+              system,
+              bundle
+            ];
+            return nameToUrl(normalize(ar), ext, true);
+          }
+        };
+      return (
+        mixin(tool(modMap, o, NAME).parser, namer) &&
+        Y(
+          !modMap &&
+            (tool(modMap, o, NAME).parser.undef = (id) => {
+              tkeGblQue(); //Only allow undef when top level require calls
+              var map = makeModuleMap(id, modMap, true), //Bind define() calls (fixes #408) to 'thi' STATE
+                m = e_(STATE.dependencies).yes(id) && STATE.dependencies[id];
+
+              return (
+                (m.undefed = true) &&
+                rmvScrpt(id, STATE.NAME) &&
+                KeyValue(`defined.${id}`, null, "delete") &&
+                KeyValue(`urlFchd.${map.url}`, null, "delete") &&
+                KeyValue(`unDE.${id}`, null, "undefined") &&
+                nextDef(id) && //Clean queued defines, backwards, so splices don't destroy the iteration
+                KeyValue(`defQueueMap.${id}`, null, "delete") &&
+                KeyValue(
+                  `unDE.${id}`,
+                  m && m.events.defined ? m.events : STATE.unDE[id]
+                ) && //if different STATE.CONFIG, same listeners
+                m &&
+                clrRegstr(id)
+              );
+            })
+        ) &&
+        tool(modMap, o, NAME).parser
+      );
+    }
+  };
+}
+
+var clrsec,
+  watch,
+  _K = (o) => (o && o.constructor === Object ? Object.keys(o) : []),
+  mixin = (tgt, s, frc, dSM) =>
+    _K(s).reduce(e_([s, tgt, frc, dSM]).reducer(), tgt),
+  rmvScrpt = (name, NAME) => {
+    const ga = "getAttribute",
+      e = (m) => (m ? name : NAME); //scriptNode
+    return (
+      isBrowser &&
+      Y(
+        e_()
+          .tag()
+          .forEach(
+            (sN) =>
+              sN[ga](dr(true)) === e(true) &&
+              sN[ga](dr()) === e() &&
+              sN.parentNode.removeChild(sN)
+          )
+      )
+    );
+  };
+export function checkLoaded(parentThis = arguments[0]) {
+  const {
+    CONFIG,
+    startTime,
+    dependencies,
+    defined,
+    enabledRegistry,
+    NAME
+  } = this;
+  var err,
+    fb,
+    hs = [],
+    reqCalls = [],
+    wait = false,
+    another = true,
+    sec = CONFIG.waitSeconds * 1000,
+    halt = sec && startTime + sec < new Date().getTime(); //It is possible to disable the wait interval by using waitSeconds of 0.
+
+  // waitInterval - Do not bother if thi call was a result of a cycle break.  hoist-"mixin" functional obj[prop]  traced,processed
+  if (watch) return true;
+  var dep,
+    progress = (
+      { m, depMaps, id, tt, p } = (m) =>
+        (id = m.map.id) && { m, depMaps: m.depMaps, id, tt: { id }, p: {} }
+    ) =>
+      Y(
+        depMaps
+          .map((map) => map.id)
+          .forEach(
+            (id, i) =>
+              (dep = e_(dependencies).yes(id) && dependencies[id]) &&
+              !m.depMatched[i] &&
+              !p[id] && // depMap force undefined (registered yet not matched in thi)
+              (!e_(tt).yes(id) || !tt[id]
+                ? progress(dep, tt, p)
+                : ["defineDep", "check"].forEach((cd, n) =>
+                    m[cd](n === 0 && (i, defined[id]))
+                  )) //pass false?
+          )
+      ) && (p[id] = true),
+    isWebWorker = !isBrowser && false, // && T(importScripts !== _n),
+    brwr = isBrowser || isWebWorker;
+  watch = true;
+
+  const _e = "error",
+    _em = "emit",
+    erro = _e; //no keys, -fails
+  console.log("In Checkloaded", "STATE reduced for purpose: ", this);
+  _K(enabledRegistry).forEach(
+    (
+      { id, noCyc } = (mod = (x) => enabledRegistry[x]) =>
+        ((
+          { yesdef, fetched, prefix, error, enabled, inited } = (map) => map
+        ) => {
+          if (enabled && !yesdef) reqCalls.push(mod);
+          mod.noCyc = fetched && yesdef && !prefix;
+          return !inited && enabled && !error ? mod : {};
+        })(mod.map)
+    ) =>
+      id && halt && !hasPathFallback(id, CONFIG.paths)
+        ? rmvScrpt(id, NAME) && hs.push(id)
+        : id &&
+          iifeapp(this)(
+            ["fb", "wait", "another"],
+            halt && true,
+            true,
+            !halt && noCyc ? false : another
+          )
+  ); //non-plugin-resource; Figure out the state of all the modules.//disabled or in error
+  if (halt && hs.length) {
+    // prettier-ignore
+    err = mk(["setTimeout", "Load setTimeout for modules: " + hs, null, hs]); //type, msg, err, requireModules
+    err.NAME = NAME;
+    return onError(err); //If wait time expired, throw error of unloaded modules.
+  }
+  return iifeapp(this)(
+    () =>
+      another &&
+      reqCalls
+        .map((require) => parentThis[require])
+        .forEach((require) =>
+          require[erro] ? require[_em](erro, require[erro]) : progress(require)
+        ), //construction
+    ["watch", "clrsec"], //keys,...values
+    false,
+    (!halt || fb) &&
+    wait &&
+    brwr &&
+    !clrsec && //call uses this or prototype, really, with ...args to follow
+      setTimeout(() => checkLoaded(this) && null, 50) /*plugin-resource*/
+  ); //args'-mutable iife=>"app"
+}
+
+const _p = "packages",
+  _b = "bundles",
+  _s = "shim",
+  _l = "location",
+  _u = "baseUrl",
+  _a = "urlArgs",
+  _xf = "exportsFn",
+  Ar = "[object Array]";
+export const configure = (
+  c = (c) => {
+    const r = T(c[_a] === _t)
+      ? (id, url) => (url.indexOf("?") === -1 ? "?" : "&") + c[_a]
+      : c[_a];
+
+    return c[_u].charAt(c[_u].length - 1) === "/" // Convert old style urlArgs string to a function.
+      ? { ...c, [_a]: r }
+      : { ...c, [_u]: `${c[_u]}/`, [_a]: r };
+  },
+  KeyValue,
+  makeModuleMap,
+  STATE,
+  mixin,
+  e_
+) => {
+  //const objs = function (){arguments.forEach(x=>thi[x]=true)}.apply({},["paths","bundles","STATE.CONFIG","map"]);
+  _K(c).forEach((prop = (op) => {
+    const arr = ["paths", "bundles", "config", "map"];
+    return Y(!arr.includes(op) ? KeyValue(`CONFIG.${op}`, c[op]) : arr.forEach(
+              (op) =>
+                KeyValue(
+                  `CONFIG.${op}`,
+                  !STATE.CONFIG[op] ? {} : STATE.CONFIG[op]
+                )
+            )) && op; //args prop
+  }, i) => mixin(STATE.CONFIG[prop], c[prop], true, true));
+
+  const mend = (bundles, shims) => {
+      var shim = STATE.CONFIG.shim; //save paths for special "additive processing"
+
+      bundles &&
+        _K(bundles).forEach((prop, i) =>
+          bundles[prop].forEach((v) =>
+            KeyValue(`bdlMap.${v}`, v !== prop ? prop : STATE.bdlMap[v])
+          )
+        ); //Reverse map the bundles
+      Y(
+        shims &&
+          _K(shims).forEach((id, i) => {
+            var temp = shims[id]; //'temp' = tobeshim
+            return (
+              Y(e_(temp).string() === Ar && (temp = { REM: temp })) && //Merge shim, Normalize the structure
+              Y(
+                (temp.exports || temp[_i]) &&
+                  !temp[_xf] &&
+                  (temp[_xf] = STATE.makeShimExports(temp))
+              ) &&
+              (shim[id] = temp)
+            );
+          })
+      );
+      return { shim, shims };
+    },
+    { shims, shim } = mend(c[_b], c[_s]);
+  return (
+    Y(KeyValue(`CONFIG.${shim}`, shims ? shim : STATE.CONFIG.shim)) &&
+    Y(
+      (!c[_p] ? [] : c[_p]).forEach((pkgObj) => {
+        pkgObj = T(pkgObj === _t) ? { name: pkgObj } : pkgObj;
+        var name = pkgObj.name,
+          location = pkgObj[_l]; //Adjust packages if necessary.
+        (location ? KeyValue(`CONFIG.paths.${name}`, pkgObj[_l]) : true) &&
+          KeyValue(
+            `CONFIG.bundle.${name}`,
+            `${pkgObj.name}/${(pkgObj.main || "main")
+              .replace(/^\.\//, "")
+              .replace(/\.js$/, "")}`
+          ); //normalize pkg name main thi ID pointer paths
+      })
+    ) && //Update maps for "waiting to execute" modules in the STATE.dependencies.
+    ((z) =>
+      Y(
+        _K(z).forEach(
+          (id = (id) => !z[id].inited && !z[id].map.unnormalized && id) =>
+            (z[id].map = makeModuleMap(id, null, true))
+        )
+      ))(STATE.dependencies) && //When require is STATE.defined, as a STATE.CONFIG object, before require.js is loaded,
+    (c.REM || c.cb) &&
+    STATE.require(c.REM || [], c.cb)
+  );
+};
