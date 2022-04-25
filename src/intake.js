d2c51e287b96ae89804dd39c71097ed3d752a2bd
Nick Carducci <nmcarducci@gmail.com>
2022-04-25 18:22:59 -0400

    Create intake.js

---
+++ src/intake.js
@@ -0,0 +1,20 @@
+const suffices = [".js", ".mjs", ""];
+const impor = (file) => {
+  var works;
+  suffices.forEach(function receipt(sfx = arguments[0]) {
+    works = import(`${file}${sfx}`)
+      .then()
+      .catch(() => receipt(suffices[suffices.indexOf(sfx)] + 1));
+  });
+  return new Promise((resolve) => {
+    works && resolve(works);
+  });
+};
+
+export const Intake = function (file, suffices, args) {
+  var Dependencies = impor(file, suffices);
+  args.forEach((arg) => {
+    Dependencies[arg] = Dependencies;
+  });
+  return { default: Dependencies, ...Dependencies };
+};
