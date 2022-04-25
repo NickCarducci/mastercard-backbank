b006040f1fc9616a16b0c10b0cf2abe9c039b8c7
Nick Carducci <nmcarducci@gmail.com>
2022-04-25 18:52:56 -0400

    Update intake.js

--- src/intake.js
+++ src/intake.js
@@ -1,12 +1,12 @@
 const suffices = [".js", ".mjs", ""];
-const impor = (file) => {
+const impor = async (file) => {
   var works;
   suffices.forEach(function receipt(sfx = arguments[0]) {
     works = import(`${file}${sfx}`)
       .then()
       .catch(() => receipt(suffices[suffices.indexOf(sfx)] + 1));
   });
-  return new Promise((resolve) => {
+  return await new Promise((resolve) => {
     works && resolve(works);
   });
 };
