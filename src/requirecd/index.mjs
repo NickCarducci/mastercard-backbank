dbd322a94e269944ca73e997b9047bb31de592f5
Nick Carducci <nmcarducci@gmail.com>
2022-04-28 20:20:25 -0400

    Create index.mjs

---
+++ src/requirecd/index.mjs
@@ -0,0 +1,554 @@
+import Dependency /*, { defineables, SETDEFINABLES }*/ from "./dependency";
+import {
+  dr,
+  iifeapp,
+  mk,
+  nameToUrl,
+  normalize,
+  reduce
+} from "./dependency/module/functions";
+
+const _S = Object.prototype.toString,
+  _H = "hasOwnProperty",
+  createElement = (ns) =>
+    document[`createElementNS${ns ? "NS" : ""}`](
+      ns ? ("http://www.w3.org/1999/xhtml", "html:script") : "script"
+    ),
+  _oE = "onError",
+  _e = "error",
+  _em = "emit",
+  _ev = "events";
+export const KeyValue = (key, value, delet) =>
+    delet === "delete"
+      ? delete buildable[key]
+      : !key.includes(".")
+      ? (buildable[key] = value)
+      : (buildable[key.split(".")[0]][key.split(".")[1]] = value),
+  SETSTATE = (STAT) => (buildable = STAT),
+  e_ = (obj /*,string*/) => {
+    /* !obj && console.log(obj + " error obj in ", thi);*/
+    const n = (NS) => NS.constructor === "String" && NS.toUpperCase() === "NS",
+      yes = (name) => obj[_H](name) /*[_P]*/,
+      string = () => _S(obj),
+      tag = (ind) => document.getElementsByTagName(obj ? obj : "script")[ind];
+    return {
+      yes,
+      reducer: (prop, nextProp) => {
+        const go =
+          obj[3] &&
+          T(obj[0][prop] === "object") &&
+          obj[0][prop] &&
+          !e_(obj[0][prop]).a() &&
+          !e_(obj[0][prop]).string() === Fn &&
+          !(obj[0][prop] instanceof RegExp);
+
+        return !obj[0]
+          ? obj[1]
+          : (obj[2] || !e_(obj[1]).yes(prop)) &&
+              (obj[1][prop] = !go ? obj[0][prop] : obj[1][prop] || {}) &&
+              mixin(obj[1][prop], obj[0][prop], obj[2], obj[3]) &&
+              obj[1];
+      }, //s,tgt,frc,dSM
+      create: (ns = n) => createElement(ns),
+      string,
+      a: (x) => x.string() === Ar,
+      tag,
+      interA: (x) => x.readyState === "interactive"
+    };
+  },
+  onError = (err = mk, eb = (eb) => eb && eb(err)) => {
+    const iserror = (err) =>
+      e_(buildable.dependencies).yes(err) && buildable.dependencies[err];
+    /*reduce when finishes with mutable object, "all" errors -
+     shallow? (like filter but with for - or mixin?)*/
+    !err.ids.reduce(
+      (
+        md = (es = iserror) => {
+          return { ...es, err };
+        } //event, event.error, emit
+      ) => md[_ev] && md[_ev][_e] && md[_em](_e, err) && true
+    ) && buildable[_oE](err);
+  },
+  hasPathFallback = (id, cP) => {
+    var pC = e_(cP).yes(id) && cP[id]; //pathConfig,configPaths
+    if (pC && e_(pC).string() === "[object Array]" && pC.length > 1) {
+      pC.shift(); //config is live? but 'id' is variable as args.. [for the?] next try
+      buildable.requir.undef(id);
+      buildable.makeRequire(null, { skipMap: true })([id]);
+      return true;
+    }
+  };
+
+var T = (x) => typeof x,
+  Y = (value, z, _) => {
+    if (z && _) z[_] = value;
+    return true;
+  }, //seratimNull
+  _n = "undefined",
+  navigator = _n,
+  buildable = (Build) => function () {}.bind(Build), //well-characterized safety profiles - returns a function, how apropos of bind with a fat arrow
+  //_x = "exports",ga = "getAttribute",
+  //scriptPends,
+  _t = "string",
+  interscrpt,
+  useInteractive = false,
+  contexts = {};
+//prettier-ignore
+/*const define = (/*requir|exports/module*{ nm, rem, c, n } = ( nmREMc = (nm, rem, c) => T(nm !== _t)? { rem: nm, c: rem }: e_(rem).string() !== Ar? { nm, rem, c }: { nm, c: rem }
+    ) => {  return { ...nmREMc, n: scriptPends ||(() => { if (interscrpt && e_(interscrpt).interA()) return interscrpt; e_().tag().sort((a, b) => b - a).map((script) => e_(script).interA() && (interscrpt = script));
+            return interscrpt; })()};}) =>Y(rem =!rem && e_(c).string() === Fn && c.length? ((
+              { rem, cb } = (rem, cb) => {return {cb: cb.toString().replace( /\/\*[\s\S]*?\*\/|([^:"'=]|^)\/\/.*$/gm /*comment *,
+                      (match, singlePrefix) => singlePrefix || "").replace(/[^.]\s*require\s*\(\s*["']([^'"\s]+)["']\s*\)/g /*requires *,(match, dep) => rem.push(dep)),rem};} /*like ')//comment'; keep prefix*) =>
+              (cb.length === 1 ? ["require"] : ["require", _x, _m]).concat(rem))(rem, c): rem) && Y((nm = useInteractive && !nm ? n()[ga](dr(true)) : nm)) && Y((buildable = useInteractive ? contexts[n()[ga](dr())] : buildable)) &&(!buildable ? SETDEFINABLES([...defineables, [nm, rem, c]]) : true) && buildable.defQueue.push([nm, rem, c]) &&(buildable.defQueueMap[nm] = true) && { amd: { jQuery: true } };
+Potential-CommonJS use-case of exports and thi, without 'requir.'; 
+     no deps nor name + cb is func => then CommonJS, iifeapp(["interscrpt"], "value");
+     getInteractiveScript Look for a data-main script attribute, which could also 
+     adjust the baseUrl. baseUrl from script tag with requir.js in it.
+     */
+
+/**UNLICENSED BUT FOR PARTS OF OTHERS */
+
+/** vim: et:ts=4:sw=4:sts=4
+ * @license RequireJS 2.3.6.carducci Copyright jQuery Foundation and other contributors.
+ * Released under MIT license, https://github.com/REQUIREJS/REQUIREJS/blob/master/LICENSE
+ */
+/*Not using strict: uneven strict support in browsers, #392, and causes
+problems with REQUIREJS.exec()/transpiler plugins that may not be strict.
+jslint regexp: true, nomen: true, sloppy: true 
+dependency window, navigator, document, importScripts, setTimeout, opera 
+cannot thi never get to the string regex?*/
+const  Ar = "[object Array]",
+  Fn = "[object Function]",
+  _K = (o) => (o && o.constructor === Object ? Object.keys(o) : []),
+  mixin = (tgt, s, frc, dSM) =>
+    _K(s).reduce(e_([s, tgt, frc, dSM]).reducer(), tgt);
+
+export function applyREQUIREJS(
+  REM = arguments[0], //String(requir|export|module)
+  cb = arguments[1],
+  eb = arguments[2],
+  optional = arguments[3]
+) {
+  /*String(requir|export|module)
+          Caja compliant buildable for minified-scope name of dependency, cb for arr completion Find the right buildable, use default*/
+  var NAME = _,
+    Build,
+    config;
+  if (!e_(REM).string() === Ar && T(REM !== _t)) {
+    config = REM;
+    return !e_(cb).a()
+      ? (REM = [])
+      : (REM = cb) && (cb = eb) && (eb = optional);
+  } /* Determine if have Build.CONFIG object in the call. REM is a Build.CONFIG object
+           Adjust args if there are Build.dependencies console.log("Build, requir", buildable, Build && Build.requir);*/
+  (NAME = config && config.context ? config.context : NAME) &&
+    Y((Build = e_(contexts).yes(NAME) && contexts[NAME]));
+
+  if (!Build) {
+    const checkProto = reduce.call(
+        this,
+        [
+          "CONFIG",
+          "startTime",
+          "dependencies",
+          "defined",
+          "enabledRegistry",
+          "NAME"
+        ],
+        "Build",
+        Build
+      ),
+      config = {
+        Build,
+        buildable,
+        makeModuleMap,
+        KeyValue,
+        Y,
+        checkProto,
+        moduleProto: { makeModuleMap, useInteractive, _e }
+      };
+    contexts[NAME] = Build = Dependency.bind(
+      config
+    ); /*context, newContext, bundlesMap; call is like prototype*/
+  }
+  return (
+    Y(
+      config &&
+        configure(
+          config,
+          KeyValue,
+          makeModuleMap,
+          reduce.call(
+            this,
+            ["CONFIG", "bdlMap", "makeShimExports", "dependencies", "requir"],
+            "Build",
+            Build
+          ),
+          mixin,
+          e_
+        )
+    ) && Build.requir(REM, cb, eb)
+  );
+}
+export default function () {
+  console.log("default index.mjs, try DurableObjectExample");
+}
+
+//[], () => d, null,{enabled: true,ignore: true} if multiple define calls for the same thi
+
+var /*
+      buildable.requir.undef(id);
+      buildable.makeRequire(null, { skipMap: true })([id]);
+      buildable = buildable ? buildable : (contexts[NAME] = new buildable.start.Dependency(NAME)); //dependency
+      config && buildable.configure(config);
+      return buildable.requir(REM, cb, eb);
+    */
+  _ = "_",
+  _u = "baseUrl";
+/*'applyMap' for dependency ID, 'baseName' relative to 'name,' the most relative
+    uses 'thi' as 'z', but when called () the function is returned,*/
+function makeModuleMap(
+  n = arguments[0],
+  sourcemap = arguments[1],
+  isNormed = arguments[2],
+  applyMap = arguments[3]
+) {
+  var ptName = sourcemap ? sourcemap.name : null,
+    givenName = n,
+    yesdef = true; /*n, sourcemap, isNormed, applyMap
+      'applyMap' for dependency ID, 'isNormed' define() thi ID, '[sourcemap]' 
+      to resolve relative names (&& requir.normalize()), 'name' the most relative
+      internally-name a 'requir' call, given no name*/
+  n =
+    (!n ? Y((yesdef = false)) : true) &&
+    (n ? n : "_@r" + (requireCounter += 1));
+
+  const configGets = [
+      buildable.CONFIG.nodeIdCompat,
+      buildable.CONFIG.system,
+      buildable.CONFIG.bundle
+    ],
+    splitPrefix = (i = (n) => n.indexOf("!")) =>
+      i > -1 ? [n.substring(0, i), n.substring(i + 1, n.length)] : [n, ""];
+
+  var names = splitPrefix(n),
+    p = names[0],
+    pM,
+    url,
+    normed = "",
+    id,
+    suffix =
+      p && !pM && !isNormed
+        ? "_unnormalized" + (unnormalizedCounter += 1)
+        : ""; /*[plugin=undefined, resource={}] if the name without a plugin prefix.
+          If it may be a plugin id that doesn't normalization, stamp it with a unique ID*/
+  n = names[1];
+  if (n)
+    p
+      ? (normed = isNormed
+          ? n
+          : pM && pM.normalize
+          ? //prettier-ignore
+            pM.normalize(n, (n) => normalize(n, ptName, applyMap, ...configGets))
+          : n.indexOf("!") === -1
+          ? normalize(n, ptName, applyMap, ...configGets)
+          : n) && (id = p + "!" + normed + suffix)
+      : iifeapp(this)(
+          ["normed", "names", "p", "normed", "isNormed", "url", "id"],
+          normalize(n, ptName, applyMap, ...configGets),
+          splitPrefix(normed),
+          names[0],
+          names[1],
+          true,
+          (nameToUrl(normed).prototype = {
+            CONFIG: buildable.CONFIG,
+            bdlMap: buildable.bdlMap
+          }),
+          normed + suffix
+        );
+
+  /*do not normalize if nested plugin references; albeit thi deprecates resourceIds,
+    normalize after plugins are loaded and such normalizations allow for async loading of a loader plugin (#1131)
+    ok base name, relative path?.normalize's 'map buildable.CONFIG application' might make normalized 'name' a 
+    plugin ID.'map buildable.CONFIG values' are already normalized at thi point.*/
+  return {
+    prefix: p,
+    name: normed,
+    parentMap: sourcemap,
+    unnormalized: !!suffix,
+    url,
+    givenName,
+    yesdef,
+    id
+  }; //depMaps
+}
+const _p = "packages",
+  _b = "bundles",
+  _s = "exportable",
+  _l = "location",
+  _a = "urlArgs",
+  _xf = "exportsFn",
+  _i = "init";
+const configure = (
+  c = (c) => {
+    const r = T(c[_a] === _t)
+      ? (id, url) => (url.indexOf("?") === -1 ? "?" : "&") + c[_a]
+      : c[_a];
+
+    return c[_u].charAt(c[_u].length - 1) ===
+      "/" /* Convert old style urlArgs string to a function.*/
+      ? { ...c, [_a]: r }
+      : { ...c, [_u]: `${c[_u]}/`, [_a]: r };
+  },
+  KeyValue,
+  makeModuleMap,
+  Building,
+  mixin,
+  e_
+) => {
+  //const objs = function (){arguments.forEach(x=>thi[x]=true)}.apply({},["paths","bundles","Building.CONFIG","map"]);
+  Object.keys(c).forEach((prop = (op) => {
+    const arr = ["paths", "bundles", "config", "map"];
+    return Y(!arr.includes(op) ? KeyValue(`CONFIG.${op}`, c[op]) : arr.forEach(
+              (op) =>
+                KeyValue(
+                  `CONFIG.${op}`,
+                  !Building.CONFIG[op] ? {} : Building.CONFIG[op]
+                )
+            )) && op;
+  }, i) => mixin(Building.CONFIG[prop], c[prop], true, true));
+  /*args prop; save paths for special "additive processing;" Reverse map the bundles; 'exportable' = tobeshim*/
+  const mend = (bundles, exportables) => {
+      var exportable = Building.CONFIG.exportable;
+      bundles &&
+        Object.keys(bundles).forEach((prop, i) =>
+          bundles[prop].forEach((v) =>
+            KeyValue(`bdlMap.${v}`, v !== prop ? prop : Building.bdlMap[v])
+          )
+        );
+      Y(
+        exportables &&
+          Object.keys(exportables).forEach((id, i) => {
+            var exportable = exportables[id];
+            return (
+              Y(
+                e_(exportable).string() === Ar &&
+                  (exportable = { REM: exportable })
+              ) && //Merge exportable, Normalize the structure
+              Y(
+                (exportable.exports || exportable[_i]) &&
+                  !exportable[_xf] &&
+                  (exportable[_xf] = Building.makeShimExports(exportable))
+              ) &&
+              (exportable[id] = exportable)
+            );
+          })
+      );
+      return { exportable, exportables };
+    },
+    { exportables, exportable } = mend(c[_b], c[_s]);
+  return (
+    Y(
+      KeyValue(
+        `CONFIG.${exportable}`,
+        exportables ? exportable : Building.CONFIG.exportable
+      )
+    ) &&
+    Y(
+      (!c[_p] ? [] : c[_p]).forEach((pkgObj) => {
+        pkgObj = T(pkgObj === _t) ? { name: pkgObj } : pkgObj;
+        var name = pkgObj.name,
+          location = pkgObj[_l]; //Adjust packages if necessary.
+        (location ? KeyValue(`CONFIG.paths.${name}`, pkgObj[_l]) : true) &&
+          KeyValue(
+            `CONFIG.bundle.${name}`,
+            `${pkgObj.name}/${(pkgObj.main || "main")
+              .replace(/^\.\//, "")
+              .replace(/\.js$/, "")}`
+          ); /*normalize pkg name main thi ID pointer paths; Update maps for
+          "waiting to execute" modules in the Building.dependencies.
+          When requir is Building.defined, as a Building.CONFIG object, before requir.js is loaded,*/
+      })
+    ) &&
+    ((z) =>
+      Y(
+        Object.keys(z).forEach(
+          (id = (id) => !z[id].inited && !z[id].map.unnormalized && id) =>
+            (z[id].map = makeModuleMap(id, null, true))
+        )
+      ))(Building.dependencies) &&
+    (c.REM || c.cb) &&
+    Building.requir(c.REM || [], c.cb)
+  );
+};
+/* T(importScripts !== _n),
+  'loading', 'loaded', execution, 'complete'
+  Oh the tragedy, detecting opera. See the usage of isOpera for reason.*/
+const isBrowser = false,
+  readyRegExp =
+    isBrowser && navigator.platform === "PLAYSTATION 3"
+      ? /^complete$/
+      : /^(complete|loaded)$/,
+  isOpera = false; //T(opera !== _n) && opera.toString() === "[object Opera]";
+/*  
+          e_
+        mixin
+        mk
+        concat
+    
+        requir=(dep,to)=>{define,configuration(config?!requir,buildable),
+          convertName
+          rmvScript
+          hasPathFallback
+          parseName
+          normalize
+          thi
+          Module
+          buildable
+          obj
+    
+          requir=buildable
+          Dependency = {
+            buildable:{buildable.CONFIG}
+            buildable.dependencies
+            makeModuleMap
+            getModule
+            when
+            onError
+            handlers
+            clrRegstr
+            checkLoaded
+            init
+            normalizeMod
+            Module[_P]={init,defineDep,fetch,load,check,callPlugin,enable,when,emit}
+            callGetModule
+            getScriptData
+            tkeGblQue
+            evt
+            buildable:{…initial:{buildable.CONFIG}}
+            buildable.requir = buildable.makeRequire()
+            return buildable
+          }
+          
+          s = buildable.start
+          buildable({})
+          urlUnDeSpec
+          head
+          onError,createNode,load
+          exec 
+          buildable()
+        }
+        */
+console.log(
+  "In Require: ",
+  "makeModuleMap",
+  "configure",
+  makeModuleMap,
+  configure
+);
+
+var requireCounter = 1,
+  unnormalizedCounter = 1,
+  evt = (
+    v = (evt) =>
+      evt.type === "load" ||
+      readyRegExp.test((evt.currentTarget || evt.srcElement).readyState)
+  ) =>
+    Y("undefined"((interscrpt = v ? null : interscrpt))) &&
+    v &&
+    getScriptData(evt), //interactiveScript - browser event for script loaded status
+  onScriptLoad = (data = evt) => buildable.completeLoad(data.id),
+  onScriptError = (evt) => {
+    var data = getScriptData(evt);
+    if (!hasPathFallback(data.id, buildable.CONFIG.paths)) {
+      const parents = _K(buildable.dependencies)
+        .map((key, i) =>
+          key.indexOf("_@r") !== 0
+            ? buildable.dependencies[key].depMaps.forEach((depMap) =>
+                depMap.id === data.id ? key : ""
+              )
+            : ""
+        )
+        .filter((x) => x !== "");
+      return onError(
+        mk([
+          "scripterror",
+          `Script error for ${
+            // prettier-ignore
+            data.id + (parents.length ? `" needed by: ${parents.join(", ")}` : '"')
+          }`,
+          evt,
+          [data.id]
+        ])
+      );
+    }
+  },
+  getScriptData = (
+    { rm, n } = (evt) => {
+      return {
+        rm: (node, func, name, ieName) =>
+          !node.detachEvent || isOpera
+            ? node.removeEventListener(name, func, false)
+            : ieName && node.detachEvent(ieName, func),
+        n: evt.currentTarget || evt.srcElement //REQUIREJS event info, remove listener from node //target
+      };
+    }
+  ) =>
+    rm(n, onScriptLoad, "load", "onreadystatechange") &&
+    rm(n, onScriptError, _e) && {
+      node: n,
+      id: n && n.getAttribute(dr(true))
+    };
+const sign = { version: "2.3.6.carducci", isBrowser: false };
+var REQUIREJS = applyREQUIREJS.bind(sign);
+
+buildable.NAME = null;
+buildable.CONFIG = {
+  waitSeconds: 7,
+  baseUrl: "./", //bundle used to be packages
+  ...["paths", "bundles", "bundle", "exportable", "config"].map((x) => {
+    return { [x]: {} };
+  })
+};
+console.log("In Require: ", "buildable", buildable);
+buildable({});
+console.log("In Require: ", "buildable(.start)", buildable.start);
+buildable.start = { contexts };
+Y(
+  ["toUrl", "undef", "defined", "specified"].forEach(
+    (prop) =>
+      (buildable[prop] = function () {
+        //apply a meaningless initial this._ state to a requir function
+        return contexts[_].requir[prop].apply(contexts[_], arguments);
+      })
+  )
+);
+//&&(buildable.load =
+var requir;
+/*T(define === _n) ||*/
+if (T(REQUIREJS === _u) || e_(REQUIREJS).string() !== Fn) {
+  requir = buildable; // package-names, cb, returns a value to define the thi of argument index[0]
+} else
+  requir = function () {
+    var configuration = {},
+      noSetTimeout,
+      setTimeout = T(noSetTimeout === "undefined") ? undefined : noSetTimeout;
+
+    //dependency = arguments[0], //T(requir === _n) || e_(requir).string() === Fn;
+    const notrequire = true,
+      notBaseUrl = T(REQUIREJS !== _u),
+      c = notBaseUrl ? (REQUIREJS ? notrequire : requir) : null,
+      r = notBaseUrl ? (undefined ? notrequire : undefined) : null;
+    Y((configuration = c)) && Y((REQUIREJS = r));
+    //(name,baseName,applyMap,configNodeIdCompat,configMap,configPkgs)
+
+    buildable.CONFIG = (config) => buildable(config);
+    buildable.nextTick = (fn) =>
+      T(setTimeout !== _n) ? setTimeout(fn, 4) : fn();
+    // globally agreed names for other potential AMD loaders
+
+    // if (!requir) requir = buildable; //Exportable requir
+    return buildable(configuration);
+  };
+
+export { requir };
