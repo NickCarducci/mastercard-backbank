5d7ff46aa9e2bbadf651624d1be6f4cfa455f54b
Nick Carducci <nmcarducci@gmail.com>
2022-04-30 18:49:12 -0400

    Update index.mjs

--- src/requirecd/index.mjs
+++ src/requirecd/index.mjs
@@ -384,37 +384,41 @@ Y(
   )
 );
 //&&(buildable.load =
-var requir;
+//var requir;
 /*T(define === _n) ||*/
 //if (T(REQUIREJS === _u) || e_(REQUIREJS).string() !== Fn) {
 //requir = buildable; // package-names, cb, returns a value to define the thi of argument index[0]
 //} else
 
-requir = new Promise(
-  (resolve) =>
-    T(REQUIREJS !== _u) &&
-    e_(REQUIREJS).string() === Fn &&
-    resolve(function () {
-      var configuration = {},
-        noSetTimeout,
-        setTimeout = T(noSetTimeout === "undefined") ? undefined : noSetTimeout;
-      //dependency = arguments[0], //T(requir === _n) || e_(requir).string() === Fn;
-      const notrequire = true,
-        notBaseUrl = T(REQUIREJS !== _u),
-        c = notBaseUrl ? (REQUIREJS ? notrequire : requir) : null,
-        r = notBaseUrl ? (undefined ? notrequire : undefined) : null;
-      Y((configuration = c)) && Y((REQUIREJS = r));
-      //(name,baseName,applyMap,configNodeIdCompat,configMap,configPkgs)
+function requir() {
+  return new Promise(
+    (resolve) =>
+      T(REQUIREJS !== _u) &&
+      e_(REQUIREJS).string() === Fn &&
+      resolve(function () {
+        var configuration = {},
+          noSetTimeout,
+          setTimeout = T(noSetTimeout === "undefined")
+            ? undefined
+            : noSetTimeout;
+        //dependency = arguments[0], //T(requir === _n) || e_(requir).string() === Fn;
+        const notrequire = true,
+          notBaseUrl = T(REQUIREJS !== _u),
+          c = notBaseUrl ? (REQUIREJS ? notrequire : requir) : null,
+          r = notBaseUrl ? (undefined ? notrequire : undefined) : null;
+        Y((configuration = c)) && Y((REQUIREJS = r));
+        //(name,baseName,applyMap,configNodeIdCompat,configMap,configPkgs)
 
-      buildable.CONFIG = (config) => buildable(config);
-      buildable.nextTick = (fn) =>
-        T(setTimeout !== _n) ? setTimeout(fn, 4) : fn();
-      // globally agreed names for other potential AMD loaders
+        buildable.CONFIG = (config) => buildable(config);
+        buildable.nextTick = (fn) =>
+          T(setTimeout !== _n) ? setTimeout(fn, 4) : fn();
+        // globally agreed names for other potential AMD loaders
 
-      const requi = buildable(configuration);
-      console.log("requir/buildable_CONFIG_nextTick(configuration)", requi);
-      // if (!requir) requir = buildable; //Exportable requir
-      return requi;
-    })
-);
+        const requi = buildable(configuration);
+        console.log("requir/buildable_CONFIG_nextTick(configuration)", requi);
+        // if (!requir) requir = buildable; //Exportable requir
+        return requi;
+      })
+  );
+}
 export { requir };
