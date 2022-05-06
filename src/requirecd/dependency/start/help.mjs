4e77709c9a4442775cbadc453572ed5959bcde54
Nick Carducci <nmcarducci@gmail.com>
2022-05-06 17:00:58 -0400

    Create help.mjs

---
+++ src/requirecd/dependency/start/help.mjs
@@ -0,0 +1,109 @@
+import { KeyValue, onError } from "../../..";
+import { mk, reduce, tryCatch } from ".";
+import { normalize } from "../utils/normalize";
+import { nameToUrl } from "../utils/nametourl";
+
+export default function Help(
+  e_ = arguments[0],
+  dependency = arguments[1],
+  BUILD = arguments[2],
+  s
+) {
+  const { moduleProto } = this,
+    {
+      Module,
+      CONFIG: config = (CONFIG) => CONFIG.config,
+      urlFchd,
+      load
+    } = dependency,
+    clrRegstr = (id) =>
+      KeyValue(`dependencies.${id}`, null, "delete") &&
+      KeyValue(`enabledRegistry.${id}`, null, "delete"),
+    depMap = (dm) => {
+      return {
+        dm,
+        m:
+          e_(dependency.dependencies).yes(dm.id) &&
+          dependency.dependencies[dm.id]
+      };
+    };
+  return {
+    getModule: ({ m, dm } = depMap) => {
+      const manage = (map, ignore, id, depMaps) =>
+          map.yesdef && !ignore
+            ? new Promise((resolve) =>
+                KeyValue(`defined.${id}`, module.exports && resolve(""))
+              ).then(
+                () =>
+                  BUILD.onResourceLoad &&
+                  BUILD.onResourceLoad(
+                    dependency,
+                    map,
+                    depMaps.map((depMap) => depMap.normalizedMap || depMap)
+                  )
+              )
+            : true,
+        mold = (id, tkn) =>
+          e_(config).yes(id)
+            ? KeyValue(`CONFIG.config.${tkn}`, config[id])
+            : true,
+        loadd = () =>
+          !urlFchd[dm.url]
+            ? KeyValue(`urlFchd.${dm.url}`, true) && load(dm.id, dm.url)
+            : console.log(
+                `redundant dependency.load(${dm.id}, ${dm.url}) call (?), dependency.urlFchd[${dm.url}] === true`
+              ),
+        execute = (text, id) => {
+          const erro = tryCatch(BUILD.exec, [text]);
+          if (erro)
+            return onError(
+              mk([
+                "fromtexteval",
+                `fromText eval for ${id} failed: ${erro}`,
+                erro,
+                [id]
+              ])
+            );
+        },
+        matches = [
+          "unDE",
+          "CONFIG",
+          "dependencies",
+          "makeRequire",
+          "bdlMap",
+          "completeLoad",
+          "enable",
+          "execCb",
+          "defined",
+          "defQueueMap"
+        ],
+        { MakeModuleMap, useInteractive, _e } = moduleProto;
+      const Dependency = this;
+      return m
+        ? m
+        : KeyValue(
+            `dependencies.${dm.id}`,
+            new Module(
+              dm,
+              MakeModuleMap,
+              useInteractive,
+              _e,
+              clrRegstr,
+              nameToUrl,
+              Help(e_, dependency, BUILD, moduleProto, Dependency).getModule,
+              onError,
+              ...reduce.call(this, matches, "dependency", dependency),
+              manage,
+              mold,
+              loadd,
+              KeyValue(`startTime`, new Date().getTime()),
+              KeyValue(`enabledRegistry.${dm.id}`, Dependency),
+              normalize,
+              BUILD.onError !== ((err) => err),
+              execute,
+              depMap
+            )
+          );
+    }
+  };
+}
