cdf6376d1ef0bb7c8f1dedd2db4d319218444ea1
Nick Carducci <nmcarducci@gmail.com>
2022-06-06 10:23:56 -0400

    Create rollup.config.js

---
+++ src/rollup.config.js
@@ -0,0 +1,31 @@
+import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+//imports work in build actions until publishing to cloudflare worker
+
+export default {
+  input: "babelphp.js",
+  output: {
+    name: "default",
+    //exports: 'named',
+    //strict: false,
+    file: "babelphp.mjs",
+    format: "umd",//es
+    //sourcemap: true
+  },
+  plugins: [
+    nodePolyfills(),
+    commonjs({
+      /*
+      depreciated, done automatically, not the problem!
+      namedExports: {
+        'node_modules/picomatch/index.js': ['pm']
+      },*/
+     //exclude: ["node_modules/**/*", "notes/**/*", "src/notes/**/*"],
+     //include: "src/**/*",
+    }),
+    nodeResolve({browser:true}),
+    terser()
+  ]
+};
