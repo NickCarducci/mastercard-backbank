6b319311226675ee825bd8ce62caef61f83dfdee
Nick Carducci <nmcarducci@gmail.com>
2022-06-09 14:58:42 -0400

    Update m4.c

--- src/PHP/GNU/m4.c
+++ src/PHP/GNU/m4.c
@@ -1,6 +1,14 @@
 //https://www.gnu.org/software/m4/
 //https://savannah.gnu.org/projects/m4/
 //https://git.savannah.gnu.org/cgit/m4.git/tree/src/m4.c?h=branch-1.4
+//pointer first struct definition declaration 
+/*
+http://www.cs.toronto.edu/~heap/270F02/node31.html
+struct node is that it includes a pointer to itself. From the 
+point-of-view of the compiler, it ensures that struct node has 
+a member that is a pointer to struct node before it has even 
+completed the statement (reached the semicolon) creating struct node
+*/
 struct macro_definition
 {
   struct macro_definition *next;
