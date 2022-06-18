4a991d488f2213c669bbb50f9e8237892047fecb
Nick Carducci <nmcarducci@gmail.com>
2022-06-17 22:15:37 -0400

    Delete emscript.sh

--- src/emscript.sh
+++
@@ -1,8 +0,0 @@
-# Using Emscripten 2.0.25 https://cloudflare.tv/event/5H5JZQgQZWQwYonKhekr80
-# Serverless WebAssembly with Cloudflare Workers, Aired on June 1, 2022 @ 2:00 – 2:30 PM (EDT)
-
-emcc -02 exec.c
-  -o index.mjs \
-  -s MODULARIZE=1 \
-  -s INVOKE_RUN=0 \
-  -s EXPORTED_RUNTIME_METHODS=["callMain"]
