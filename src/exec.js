16e5e06348192baf245e6463e99f18ca4060038f
Nick Carducci <nmcarducci@gmail.com>
2022-06-19 00:58:00 -0400

    Update exec.js

--- src/exec.js
+++ src/exec.js
@@ -6,7 +6,7 @@ var quit_ = (status, toThrow) => {
   throw toThrow;
 };
 var ENVIRONMENT_IS_WEB = typeof window == "object";
-var ENVIRONMENT_IS_WORKER = false;//typeof importScripts == "function";
+var ENVIRONMENT_IS_WORKER = false; //typeof importScripts == "function";
 var ENVIRONMENT_IS_NODE =
   typeof process == "object" &&
   typeof process.versions == "object" &&
@@ -3373,4 +3373,5 @@ if (Module["preInit"]) {
 }
 var shouldRunNow = true;
 if (Module["noInitialRun"]) shouldRunNow = false;
-export default run;
+run();
+export default Module;
