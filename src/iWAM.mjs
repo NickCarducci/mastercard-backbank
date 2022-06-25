0fec7f08bc9316f5c2109669f6e8f8ad5968e8ce
Nick Carducci <nmcarducci@gmail.com>
2022-06-25 11:58:17 -0400

    Update iWAM.mjs

--- src/iWAM.mjs
+++ src/iWAM.mjs
@@ -8,27 +8,26 @@ export default async function MasterCardPHP (request) {
   return await fetch('a.out.wasm').then(response =>
     response.arrayBuffer()//WebAssembly.instantiateStreaming(fetch('a.out.wasm'), importObject)
   ).then(bufferSource => {
-    if(!WebAssembly.validate(bufferSource)) throw {name:"not bufferable",message: "cloudflare workers buffers array 'in-house'"}
-    console.log("the buffer is WAM binary, fetch() return param arrayBuffer()=initiateStreaming()");
-    return new WebAssembly.Instance(BACKBANK_WASM,imports).exports;
+    if(!WebAssembly.validate(bufferSource)) throw {name:"not bufferable",message: "cloudflare workers buffers array 'in-house'"};
+    console.log("the buffer is WAM binary: fetch(), return param arrayBuffer()=initiateStreaming()");
+    return WebAssembly.instantiate(bufferSource,imports).instance.exports;
   })
   .catch( async err => {
-    console.log("the source is not a bufferable 'WAM binary', Instance with the emcc out[-file] .js helper");
+    console.log("the source is not a bufferable 'WAM binary': should then be already instantiable thru"+
+                                  " 'new WebAssembly.Instance.exports' emcc out[-file] ['exec'].js");
     return await Module({
-        locateFile: function(path, prefix) {
-          let url = new URL(request.url);
-          //if (path.endsWith(".mem")) // if it's a mem init file, use a custom dir
-            //return "https://mycdn.com/memory-init-dir/" + path;
-          return /*prefix*/url + path;// otherwise, use the default, the prefix (JS file's dir) + the path
+        locateFile: function(path, prefix) { // if (path.endsWith(".mem")) return "https://mycdn.com/memory-init-dir/" + path
+          let url = new URL(request.url); // if it's a mem init file, return custom dir
+          return /*prefix*/url + path;// (default) prefix dir + path
         },
         //{env:{ memory: new WebAssembly.Memory({initial: 512}) }},//32MB wasmMemory
         print: text => output += `${text}\n`,
         printErr: text => output += `${text}\n`,// Instead of downloading the .wasm file, fetch it from a global var
         instantiateWasm: (imports, callback) => {
-          //resource-binding-UI/API.
+          if(!WebAssembly.validate(BACKBANK_WASM)) return console.log({name:"not bufferable",message: "instantiable already-buffered by cf workers"});
           const inst = new WebAssembly//https://cloudflare.tv/event/5H5JZQgQZWQwYonKhekr80 7-9'
                         //wrangler.toml\(wasm_modules={BACKBANK_WASM="./backbank.wasm"};)
-                        .Instance(BACKBANK_WASM,imports);
+                        .Instance(BACKBANK_WASM,imports);//resource-binding-UI/API.
           callback(inst);
           return inst.exports;
         }
@@ -36,6 +35,6 @@ export default async function MasterCardPHP (request) {
         return {
           app: module.ccall("index", null/*return type*/, [null]/*argument types*/, request/*arguments*/, {async:"true"})
         }
-      });//'null' == object return/argument type "since the beginning of javascript" - MDN typeof
+      }).catch(err=>console.log(err.message));//'null' == object return/argument type "since the beginning of javascript" - MDN typeof
   });
 }
