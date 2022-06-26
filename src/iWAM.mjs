5dc339aa3ed3917d422f9e1828375d1d7411a39d
Nick Carducci <nmcarducci@gmail.com>
2022-06-26 14:45:38 -0400

    Update iWAM.mjs

--- src/iWAM.mjs
+++ src/iWAM.mjs
@@ -5,24 +5,26 @@ import FSA from "./fileserve.js";
 let output = "";
 // By default, stdout/stderr is output to console.log/warn
 export default async function MasterCardPHP (request) { 
-  var asmArg = { __asyncjs__openXML: () => FSA({ startIn: "./source/exec.c" }) };
-  const info = { env: asmArg, wasi_snapshot_preview1: asmArg }; //asmLibraryArg
-  //fetch the final return/arrow, 'this-deepest-function'
-  return await WebAssembly.instantiateStreaming(info/*fetch('a.out.wasm')*//*await fetch('a.out.wasm').then(response =>
-    response.arrayBuffer()//WebAssembly.instantiateStreaming(fetch('a.out.wasm'), importObject)*/
-  ).then(bufferSource => {
-    if(!WebAssembly.validate(bufferSource)) throw {name:"not bufferable",message: "cloudflare workers buffers array 'in-house'"};
-    console.log("the buffer is WAM binary: fetch(), return param arrayBuffer()=initiateStreaming()");
-    return WebAssembly.instantiate(bufferSource,imports).instance.exports;
-  })
-  .catch( async err => {
-    console.log("the source is not a bufferable 'WAM binary': should then be already instantiable thru"+
-                                  " 'new WebAssembly.Instance.exports' emcc out[-file] ['exec'].js");
-    if(!WebAssembly.validate(BACKBANK_WASM)) return console.log({name:"not bufferable",message: "instantiable already-buffered by cf workers"});
+  
     async function app () {
+      var asmArg = { __asyncjs__openXML: () => FSA({ startIn: "a.out.wasm" }) };
+      const info = { env: asmArg, wasi_snapshot_preview1: asmArg }; //asmLibraryArg
+      //fetch the final return/arrow, 'this-deepest-function'
+      return await WebAssembly.instantiateStreaming(info/*fetch('a.out.wasm')*//*await fetch('a.out.wasm').then(response =>
+        response.arrayBuffer()//WebAssembly.instantiateStreaming(fetch('a.out.wasm'), importObject)*/
+      ).then(bufferSource => {
+        if(!WebAssembly.validate(bufferSource)) throw {name:"not bufferable",message: "cloudflare workers buffers array 'in-house'"};
+        console.log("the buffer is WAM binary: fetch(), return param arrayBuffer()=initiateStreaming()");
+        return WebAssembly.instantiate(bufferSource,arguments).instance.exports;
+      })
+      .catch( async err => {
+        console.log("the source is not a bufferable 'WAM binary': should then be already instantiable thru"+
+                                      " 'new WebAssembly.Instance.exports' emcc out[-file] ['exec'].js");
+        if(!WebAssembly.validate(this)) return console.log({name:"not bufferable",message: "instantiable already-buffered by cf workers"});
       return await new WebAssembly.Instance(this,arguments).exports;//callback(inst);
+      });
     }
-    return app.apply(BACKBANK_WASM,imports);
+    return app.apply(BACKBANK_WASM,request);
     //if (Module["instantiateWasm"]) {}
  
     /*return await Module({
