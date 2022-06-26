adbd63e13b46cf96f72b8cc14917b5d70606f0e4
Nick Carducci <nmcarducci@gmail.com>
2022-06-26 14:46:55 -0400

    Update iWAM.mjs

--- src/iWAM.mjs
+++ src/iWAM.mjs
@@ -4,30 +4,44 @@ import FSA from "./fileserve.js";
 // Initialize WebAssembly module
 let output = "";
 // By default, stdout/stderr is output to console.log/warn
-export default async function MasterCardPHP (request) { 
-  
-    async function app () {
-      var asmArg = { __asyncjs__openXML: () => FSA({ startIn: "a.out.wasm" }) };
-      const info = { env: asmArg, wasi_snapshot_preview1: asmArg }; //asmLibraryArg
-      //fetch the final return/arrow, 'this-deepest-function'
-      return await WebAssembly.instantiateStreaming(info/*fetch('a.out.wasm')*//*await fetch('a.out.wasm').then(response =>
+export default async function MasterCardPHP(request) {
+  async function app() {
+    var asmArg = { __asyncjs__openXML: () => FSA({ startIn: "a.out.wasm" }) };
+    const info = { env: asmArg, wasi_snapshot_preview1: asmArg }; //asmLibraryArg
+    //fetch the final return/arrow, 'this-deepest-function'
+    return await WebAssembly.instantiateStreaming(
+      info /*fetch('a.out.wasm')*/ /*await fetch('a.out.wasm').then(response =>
         response.arrayBuffer()//WebAssembly.instantiateStreaming(fetch('a.out.wasm'), importObject)*/
-      ).then(bufferSource => {
-        if(!WebAssembly.validate(bufferSource)) throw {name:"not bufferable",message: "cloudflare workers buffers array 'in-house'"};
-        console.log("the buffer is WAM binary: fetch(), return param arrayBuffer()=initiateStreaming()");
-        return WebAssembly.instantiate(bufferSource,arguments).instance.exports;
+    )
+      .then((bufferSource) => {
+        if (!WebAssembly.validate(bufferSource))
+          throw {
+            name: "not bufferable",
+            message: "cloudflare workers buffers array 'in-house'"
+          };
+        console.log(
+          "the buffer is WAM binary: fetch(), return param arrayBuffer()=initiateStreaming()"
+        );
+        return WebAssembly.instantiate(bufferSource, arguments).instance
+          .exports;
       })
-      .catch( async err => {
-        console.log("the source is not a bufferable 'WAM binary': should then be already instantiable thru"+
-                                      " 'new WebAssembly.Instance.exports' emcc out[-file] ['exec'].js");
-        if(!WebAssembly.validate(this)) return console.log({name:"not bufferable",message: "instantiable already-buffered by cf workers"});
-      return await new WebAssembly.Instance(this,arguments).exports;//callback(inst);
+      .catch(async (err) => {
+        console.log(
+          "the source is not a bufferable 'WAM binary': should then be already instantiable thru" +
+            " 'new WebAssembly.Instance.exports' emcc out[-file] ['exec'].js"
+        );
+        if (!WebAssembly.validate(this))
+          return console.log({
+            name: "not bufferable",
+            message: "instantiable already-buffered by cf workers"
+          });
+        return await new WebAssembly.Instance(this, arguments).exports; //callback(inst);
       });
-    }
-    return app.apply(BACKBANK_WASM,request);
-    //if (Module["instantiateWasm"]) {}
- 
-    /*return await Module({
+  }
+  return app.apply(BACKBANK_WASM, request);
+  //if (Module["instantiateWasm"]) {}
+
+  /*return await Module({
         locateFile: function(path, prefix) { // if (path.endsWith(".mem")) return "https://mycdn.com/memory-init-dir/" + path
           let url = new URL(request.url); // if it's a mem init file, return custom dir
           return /*prefix* /url + path;// (default) prefix dir + path
@@ -48,6 +62,6 @@ export default async function MasterCardPHP (request) {
           app: module.ccall("index", null/*return type* /, [null]/*argument types* /, request/*arguments* /, {async:"true"})
         }
       }).catch(err=>console.log(err.message));//'null' == object return/argument type "since the beginning of javascript" - MDN typeof
-      */
-  });
+      
+  });*/
 }
