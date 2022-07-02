44143bfa98cf2a880416196843e88c9a8a7461c6
Nick Carducci <nmcarducci@gmail.com>
2022-07-02 17:50:01 -0400

    Update denotract.rs

--- src/denotract.rs
+++ src/denotract.rs
@@ -10,26 +10,26 @@ fn main() {
     let context = v8::Context::new(scoper);
     let scope = &mut v8::ContextScope::new(scoper, context);//compiler
     
-    let code = v8::String::new(scope, "'Hello' + ' World!'").unwrap();
-    let script = v8::Script::compile(scope, code, None).unwrap();
-    let result = script.run(scope).unwrap();
-
-    let result = result.to_string(scope).unwrap();
-    
-    println!("{}", result.to_rust_string_lossy(scope));//this returns like javascript
-
-    //byte from machine[binary]_code
-    let c_source = r#"
+    let bytesource = "'Hello' + ' World!'";
+    /* or byte from machine[binary]_code
+    let bytesource = r#"
           let bytes = new Uint8Array([machine_code]);
           let module = new WebAssembly.Module(bytes);
           let instance = new WebAssembly.Instance(module);
-          instance.exports.add(3, 4);
-        "#;
-    let source = v8::String::new(scope, c_source).unwrap();//"string javascript"
-    let script = v8::Script::compile(scope, source, None).unwrap();//"compile source"
+          instance.exports.add(0, 1);
+        "#;*/
+    
+    let code = v8::String::new(scope, bytesource).unwrap();//"string javascript"
+    let script = v8::Script::compile(scope, code, None).unwrap();//"compile source"
     let result = script.run(scope).unwrap();//"run script"
-    let result = result.to_uint32(scope).unwrap();//"print"
-    println!("3 + 4 = {}", result.value());
+
+    let result = result.to_string(scope).unwrap();//"print"
+    /* or byte from machine[binary]_code
+    let result = result.to_uint32(scope).unwrap();*/
+    
+    println!("{}", result.to_rust_string_lossy(scope));//this returns like javascript
+    /* or byte from machine[binary]_code
+    println!("0 + 1 = {}", result.value());*///instance.exports.add(0, 1);
   }
 
   unsafe {
