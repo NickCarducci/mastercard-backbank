ff5d5d7332b6e6956096d57bd69961957a81496d
Nick Carducci <nmcarducci@gmail.com>
2022-07-03 11:46:39 -0400

    Update ifWAM.mjs

--- src/ifWAM.mjs
+++ src/ifWAM.mjs
@@ -1,29 +1,39 @@
 import Module from "./exec.js";
 export default async function MasterCardPHP(request) {
-  async function app() {
-  if (Module["instantiateWasm"]) {}
+  /*async function app() {
+  if (Module["instantiateWasm"]) {}*/
 
   return await Module({
-        locateFile: function(path, prefix) { // if (path.endsWith(".mem")) return "https://mycdn.com/memory-init-dir/" + path
-          let url = new URL(request.url); // if it's a mem init file, return custom dir
-          return /*prefix*/url + path;// (default) prefix dir + path
-        },
-        //{env:{ memory: new WebAssembly.Memory({initial: 512}) }},//32MB wasmMemory
-        print: text => output += `${text}\n`,
-        printErr: text => output += `${text}\n`,// Instead of downloading the .wasm file, fetch it from a global var
-        instantiateWasm: (imports, callback) => {
-          if(!WebAssembly.validate(BACKBANK_WASM)) return console.log({name:"not bufferable",message: "instantiable already-buffered by cf workers"});
-          const inst = new WebAssembly//https://cloudflare.tv/event/5H5JZQgQZWQwYonKhekr80 7-9'
-                        //wrangler.toml\(wasm_modules={BACKBANK_WASM="./backbank.wasm"};)
-                        .Instance(BACKBANK_WASM,imports);//resource-binding-UI/API.
-          callback(inst);
-          return inst.exports;
-        }
-      }).then(module=>{
-        return {
-          app: module.ccall("index", null/*return type*/, [null]/*argument types*/, request/*arguments*/, {async:"true"})
-        }
-      }).catch(err=>console.log(err.message))//'null' == object return/argument type "since the beginning of javascript" - MDN typeof
-      
-  });
+    locateFile: function (path, prefix) {
+      // if (path.endsWith(".mem")) return "https://mycdn.com/memory-init-dir/" + path
+      let url = new URL(request.url); // if it's a mem init file, return custom dir
+      return /*prefix*/ url + path; // (default) prefix dir + path
+    },
+    //{env:{ memory: new WebAssembly.Memory({initial: 512}) }},//32MB wasmMemory
+    print: (text) => (output += `${text}\n`),
+    printErr: (text) => (output += `${text}\n`), // Instead of downloading the .wasm file, fetch it from a global var
+    instantiateWasm: (imports, callback) => {
+      if (!WebAssembly.validate(BACKBANK_WASM))
+        return console.log({
+          name: "not bufferable",
+          message: "instantiable already-buffered by cf workers"
+        });
+      const inst = new //wrangler.toml\(wasm_modules={BACKBANK_WASM="./backbank.wasm"};)
+      WebAssembly.Instance(BACKBANK_WASM, imports); //https://cloudflare.tv/event/5H5JZQgQZWQwYonKhekr80 7-9' //resource-binding-UI/API.
+      callback(inst);
+      return inst.exports;
+    }
+  })
+    .then((module) => {
+      return {
+        app: module.ccall(
+          "index",
+          null /*return type*/,
+          [null] /*argument types*/,
+          request /*arguments*/,
+          { async: "true" }
+        )
+      };
+    })
+    .catch((err) => console.log(err.message)); //'null' == object return/argument type "since the beginning of javascript" - MDN typeof
 }
