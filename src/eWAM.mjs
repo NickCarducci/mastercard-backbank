7d80177b9e7f406eb755e0fa390258b84e81962e
Nick Carducci <nmcarducci@gmail.com>
2022-07-02 23:56:11 -0400

    Update eWAM.mjs

--- src/eWAM.mjs
+++ src/eWAM.mjs
@@ -1,10 +1,10 @@
-import * as __asyncjs__openXML from "src/backbank.mjs";
+//import * as __asyncjs__openXML from "src/backbank.mjs";
 import _wasm from "../a.out.wasm";
 
 const _wasm_memory = new WebAssembly.Memory({ initial: 512 });
 let importsObject = {
   env: { memory: _wasm_memory },
-  "src/backbank.mjs": __asyncjs__openXML,
+  //"src/backbank.mjs": __asyncjs__openXML,
 };
 
 export default new WebAssembly.Instance(_wasm, importsObject).exports;
