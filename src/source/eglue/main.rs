f093dc9f4dbd176107a6d193e160e89f1857417a
Nick Carducci <nmcarducci@gmail.com>
2022-07-04 03:08:55 -0400

    Update main.rs

--- src/source/eglue/main.rs
+++ src/source/eglue/main.rs
@@ -1,4 +1,4 @@
 #[wasm_bindgen]
 pub fn start() -> Promise {
-  wasm_bindgen_futures::future_to_promise(async { cc::Build::new().file("exec.c").expand(); })
+  wasm_bindgen_futures::future_to_promise(async { cc::Build::new().file("src/source/exec.c").expand(); })
 }
