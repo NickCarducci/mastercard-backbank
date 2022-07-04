bcec676385ae9e489f231597f5adbb6c52f4cecc
Nick Carducci <nmcarducci@gmail.com>
2022-07-04 15:18:52 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -1,5 +1,6 @@
-#![crate_type="cdylib"] // only this bytecode not ./build.rs .. don't use binary/"bin" at all instead of binarily _serialized_ bytecode?
+//#![crate_type="cdylib"] // only this bytecode not ./build.rs .. don't use binary/"bin" at all instead of binarily _serialized_ bytecode?
 extern crate bindgen;
+
 #[wasm_bindgen]
 pub fn start() -> Promise {
   // async fn out() -> cc::Build::new().file("src/source/exec.c").expand()
