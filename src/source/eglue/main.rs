ca92cf1d115cc9270d5f1d0c12826b735b166892
Nick Carducci <nmcarducci@gmail.com>
2022-07-04 14:09:06 -0400

    Update main.rs

--- src/source/eglue/main.rs
+++ src/source/eglue/main.rs
@@ -1,6 +1,6 @@
 extern crate bindgen;
 #[wasm_bindgen]
 pub fn start() -> Promise {
-  // async fn out() -> cc::Build::new().file("src/source/exec.c").expand()::default()
+  // async fn out() -> cc::Build::new().file("src/source/exec.c").expand()
   wasm_bindgen_futures::future_to_promise(async { cc::Build::new().file("src/source/exec.c").expand()::default(); })
 }
