bf89419e0a0e85bc821e0df3d3d212f137a94eed
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 18:29:22 -0400

    Update pathify.rs

--- src/source/eglue/mods/pathify.rs
+++ src/source/eglue/mods/pathify.rs
@@ -5,9 +5,9 @@ use crate::mods::utils::iterationforeach;//mods from Cargo.toml?
 //https://stackoverflow.com/a/57453866/11711280
 //use crate certain pub members (rust)?
 use crate::pubmain::{resultable,arguments,null};
-use crate::pubmain::dev::{array};
+use crate::pubmain::dev::{pathbuf};
 pub fn pathify () -> resultable {
-  let mut input_file = array();
+  let mut input_file = pathbuf();
   fn iteration () -> null {
     input_file.push();
   }
