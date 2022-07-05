6f3bf9fb0e4e5285ee380a6126b044e4ab7bceee
Nick Carducci <nmcarducci@gmail.com>
2022-07-04 23:33:56 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -9,8 +9,8 @@ use wasm_bindgen_futures;
 use cc;
 use promise;
 
-#[wasm_bindgen]
-pub fn main() -> promise::Promise {
+#[wasm_bindgen]//pub mod main start
+fn main() -> promise::Promise {
   // async fn out() -> cc::Build::new().file("src/source/exec.c").expand()
   let Mast = async { cc::Build::new().file("src/source/exec.c").expand() = Default::default();//<i32>
   wasm_bindgen_futures::future_to_promise(Mast);
