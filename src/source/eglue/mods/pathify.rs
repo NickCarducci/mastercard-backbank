6c4e4acfa475fca1f93a53cd13ac7cb285381387
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 18:02:44 -0400

    Update pathify.rs

--- src/source/eglue/mods/pathify.rs
+++ src/source/eglue/mods/pathify.rs
@@ -4,16 +4,13 @@ use crate::mods::utils::iterationforeach;//mods from Cargo.toml?
 //[[bin]] path globals preclude `crate`
 //https://stackoverflow.com/a/57453866/11711280
 //use crate certain pub members (rust)?
-use crate::pubmain::{resultable,stringifu};
+use crate::pubmain::{resultable,arguments,null};
 pub fn pathify () -> resultable {
-  let args = stringifu::from_iter(std::env::args_os());
-  if args.len() != 2 {
-      std::process::exit(1);//"Reads rust source file, including referred modules and expands them into a single source with all modules inline"
+  fn iteration () -> null {
+    input_file.push();
   }
   let mut input_file = std::path::PathBuf::new();
-  
-  let out = iterationforeach(args, x=>input_file.push(x));
-
+  let out = iterationforeach(arguments, iteration);
   println!("{}", out);
    Ok(())
 }
