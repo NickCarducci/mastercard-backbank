79ecd678c8e3b3aa7ec8d89656232a1f0036c37d
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 02:32:02 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -7,10 +7,22 @@ use wasm_bindgen::prelude::wasm_bindgen;
 // utils
 use wasm_bindgen_futures;
 use cc;
-use crate::promise;
+//use crate::promise;
 
-#[wasm_bindgen]//pub mod main start
-pub fn main () -> promise::Promise {
+fn read () -> Result<(), syn_file_expand::Error> {
+  //https://docs.rs/syn-file-expand/latest/syn_file_expand/fn.read_full_crate_source_code.html
+  let args = Vec::<OsString>::from_iter(std::env::args_os());
+  if args.len() != 2 {
+      std::process::exit(1);//"Reads rust source file, including referred modules and expands them into a single source with all modules inline"
+  }
+  let source = syn_file_expand::read_full_crate_source_code(&args[1], |_|Ok(false))?;
+  println!("{}", source.into_token_stream());
+  Ok(())
+}
+
+#[wasm_bindgen]//pub mod main start, promise::Promise
+pub fn main () -> read().Promise {
+  
   // async fn out() -> cc::Build::new().file("src/source/exec.c").expand()
   let Mast = async { cc::Build::new().file("src/source/exec.c").expand() = Default::default();};//<i32>
   wasm_bindgen_futures::future_to_promise(Mast);
