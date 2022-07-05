1ae9143d8b45c6e3a2814aea1fa41dfb200c2441
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 18:03:18 -0400

    Update pubmain.rs

--- src/source/eglue/pubmain.rs
+++ src/source/eglue/pubmain.rs
@@ -1,5 +1,6 @@
 //what primitive type is a vector type
 pub fn main () {
+  pub std::ptr::null_mut() null;
   pub Result<(), syn_file_expand::Error> resultable;
   pub Vec::<OsString> stringifu;
   pub fn arguments -> stringifu {
