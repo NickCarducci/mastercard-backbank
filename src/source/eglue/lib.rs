e832b78d03bfc77003d43bdb031134bfdb8dd0d6
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 14:00:45 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -7,6 +7,8 @@ use wasm_bindgen::prelude::wasm_bindgen;
 // utils
 use wasm_bindgen_futures;
 use cc;
+use crate::mods::pathify;
+use crate::readfm;
 //use crate::promise;
 //use promise;//"promise-rs"
 /*error[E0774]: `derive` may only be applied to `struct`s, `enum`s and `union`s
@@ -19,27 +21,11 @@ use cc;
     |         ---------------------------------------------- not a `struct`, `enum` or `union`
 */
 
-fn read () -> Result<(), syn_file_expand::Error> {
-  //https://docs.rs/syn-file-expand/latest/syn_file_expand/fn.read_full_crate_source_code.html
-  let args = Vec::<OsString>::from_iter(std::env::args_os());
-  if args.len() != 2 {
-      std::process::exit(1);//"Reads rust source file, including referred modules and expands them into a single source with all modules inline"
-  }
-  let source = syn_file_expand::read_full_crate_source_code(&args[1], |_|Ok(false))?;
-  println!("{}", source.into_token_stream() );
-   Ok(())
-}//https://doc.rust-lang.org/std/result/
-fn namer () -> Result<(), syn_file_expand::Error> {
-  let mut input_file = std::path::PathBuf::new();
-  input_file.push("src");
-  input_file.push("source");
-  input_file.push("eglue");
-  println!("{}", input_file.push("promise.rs") );
-   Ok(())
-}
+
+
 //Result<(), syn_file_expand::Error>::
 #[wasm_bindgen]//pub mod main start, promise::Promise
-pub fn main () -> read(namer())::/*promise*/{Promise {
+pub fn main () -> readfm(pathify("src/source/promise.rs".split("/")))::/*promise*/{Promise {
   
   // async fn out() -> cc::Build::new().file("src/source/exec.c").expand()
   let Mast = async { cc::Build::new().file("src/source/exec.c").expand() = Default::default();};//<i32>
