9ddd005086eccacd5fd060ef627d5125a16551d5
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 19:29:20 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -1,33 +1,4 @@
-//#![crate_type="cdylib"] // only this bytecode not ./build.rs .. don't use binary/"bin" at all instead of binarily _serialized_ bytecode?
-//extern crate bindgen;//https://stackoverflow.com/a/50307802/11711280
-//extern crate bindgen;//mod use crate::c;
-//use bindgen;//use ::ifcrate
-// attribute
-use wasm_bindgen::prelude::wasm_bindgen;
-// utils
-use wasm_bindgen_futures;
-use cc;
-use crate::pub::mods::{pathify,dev};
-use dev::jsfuture;
-use crate::readfm;
-//use crate::promise;
-//use promise;//"promise-rs"
-/*error[E0774]: `derive` may only be applied to `struct`s, `enum`s and `union`s
-   --> /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/promise-rs-0.3.0/src/lib.rs:192:9
-    |
-192 |         #[derive(Debug)]
-    |         ^^^^^^^^^^^^^^^^ not applicable here
-193 |         // let mut pending_promises_threads: Vec<Arc<Mutex<Option<std::thread::JoinHandle<()>>>>> = vec![];
-194 |         let mut resoved_results: Vec<String> = vec![];
-    |         ---------------------------------------------- not a `struct`, `enum` or `union`
-*/
 
+use crate::{awaitfm};
 
-
-// Future<Output = Result<JsValue, JsValue>>
-#[wasm_bindgen]//pub mod main start, promise::Promise
-pub fn main () -> jsfuture {
-  let red = readfm(pathify("src/source/promise.rs".split("/")));
-  impl Future for Mast { cc::Build::new().file(red).expand() = Default::default();};//<i32>
-  wasm_bindgen_futures::future_to_promise(Mast);
-}
+pub fn main () -> awaitfm("src/source/promise");
