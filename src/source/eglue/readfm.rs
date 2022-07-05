e334dada1a25b9d8328a01b57baecae4b1777c6e
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 15:40:50 -0400

    Update readfm.rs

--- src/source/eglue/readfm.rs
+++ src/source/eglue/readfm.rs
@@ -1,8 +1,8 @@
 // a reading, module referencing, and expansion of a rust file source
 // https://docs.rs/syn-file-expand/latest/syn_file_expand/fn.read_full_crate_source_code.html
-
+use stringify;
 pub fn readfm () -> Result<(), syn_file_expand::Error> {
-  let args = Vec::<OsString>::from_iter(std::env::args_os());
+  let args = stringify::from_iter(std::env::args_os());
   if args.len() != 2 {
       std::process::exit(1);
   }
