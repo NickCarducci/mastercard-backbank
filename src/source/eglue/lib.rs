0a33c979e28cc0b9aec7fb387f1d9edb04373d68
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 19:45:00 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -1,4 +1,5 @@
-
-use crate::{awaitfm};
-
-pub fn main () -> awaitfm("src/source/promise");
+use crate::pub::jsfuture;
+use crate::awaitfm;
+//use crate::pub::arguments//gets rewritten inside deepest scope "apply" thisArg, argsArrayArg
+//fn ego () -> jsfuture awaitfm(arguments[0]);
+pub fn main () -> jsfuture awaitfm("src/source/exec.c");
