26dd6c7986ecd8af044bedd90d14744938d43ca3
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 14:06:11 -0400

    Update pathify.js

--- src/source/eglue/mods/pathify.js
+++ src/source/eglue/mods/pathify.js
@@ -1,6 +1,6 @@
 use crate::iterationforeach;
 
-fn pathify () -> Result<(), syn_file_expand::Error> {
+pub fn pathify () -> Result<(), syn_file_expand::Error> {
   let args = Vec::<OsString>::from_iter(std::env::args_os());
   if args.len() != 2 {
       std::process::exit(1);//"Reads rust source file, including referred modules and expands them into a single source with all modules inline"
