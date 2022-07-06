d6c557cd6ac5c43dd51dd4ad8dd02b97390bc792
Nick Carducci <nmcarducci@gmail.com>
2022-07-06 11:13:39 -0400

    Update pathify.rs

--- src/source/eglue/bupkis/mods/pathify.rs
+++ src/source/eglue/bupkis/mods/pathify.rs
@@ -1,11 +1,11 @@
-use crate::mods::utils::iterationforeach;//mods from Cargo.toml?
+use crate::bupkis::{resultable,arguments,null,mods,dev}
+use mods::utils::iterationforeach;//mods from Cargo.toml?
 //each crate has one main function
 //with pub uses
 //[[bin]] path globals preclude `crate`
 //https://stackoverflow.com/a/57453866/11711280
 //use crate certain pub members (rust)?
-use crate::pubmain::{resultable,arguments,null};
-use crate::pubmain::dev::{pathbuf};
+use dev::{pathbuf};
 pub fn pathify () -> resultable {
   let mut input_file = pathbuf();
   fn iteration () -> null {
