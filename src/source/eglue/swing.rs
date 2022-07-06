d6f6c40df817810fce874d3e48ec479c3e5bf488
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 23:15:48 -0400

    Update and rename readfm.rs to swing.rs

--- src/source/eglue/swing.rs
+++ src/source/eglue/swing.rs
@@ -2,8 +2,9 @@
 // https://docs.rs/syn-file-expand/latest/syn_file_expand/fn.read_full_crate_source_code.html
 //use stringify;// no Vec<Oostring> allocation needed, no methods like from_iter?
 use pubmain::{arguments,resultable};
-pub fn readfm () -> resultable {
+pub fn swing () -> resultable {
   let source = syn_file_expand::read_full_crate_source_code(&arguments[1], |_|Ok(false))?;
   println!("{}", source.into_token_stream() );
    Ok(())
 }//https://doc.rust-lang.org/std/result/
+//opens crate
