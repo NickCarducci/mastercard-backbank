4b3299e8a1e622fd54114df99e39378db279047d
Nick Carducci <nmcarducci@gmail.com>
2022-07-06 14:03:52 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -1,5 +1,5 @@
-use crate::bupkis::{jsfuture,arguments};//'arguments'/stringifu::from_iter(std::env::args_os()) gets rewritten inside deepest scope "apply" thisArg, argsArrayArg
+use crate::bupkis::{n,jsfuture,arguments};//'arguments'/stringifu::from_iter(std::env::args_os()) gets rewritten inside deepest scope "apply" thisArg, argsArrayArg
 use crate::jfmast;
 //fn ego () -> jsfuture awaitfm(arguments[0]);
 #[wasm_bindgen(start)]
-pub fn main () -> jsfuture(&jfmast(&arguments(int 0)));//"src/source/exec.c"
+pub fn main () -> jsfuture(&jfmast(&arguments(n 0)));//"src/source/exec.c"
