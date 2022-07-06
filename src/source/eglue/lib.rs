36a4f089ffd6e18c65693ded71b2a9b48949b4e6
Nick Carducci <nmcarducci@gmail.com>
2022-07-06 11:00:11 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -2,4 +2,5 @@ use crate::pub::jsfuture;
 use crate::jfmast;
 use crate::pub::arguments//gets rewritten inside deepest scope "apply" thisArg, argsArrayArg
 //fn ego () -> jsfuture awaitfm(arguments[0]);
+#[wasm_bindgen(start)]
 pub fn main () -> jsfuture jfmast(arguments[0]);//"src/source/exec.c"
