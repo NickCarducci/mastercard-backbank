7fb8339a956f5f405bed1d8b4711d36e5d1c2f6f
Nick Carducci <nmcarducci@gmail.com>
2022-07-05 23:18:52 -0400

    Update main.rs

--- src/source/eglue/pub/main.rs
+++ src/source/eglue/pub/main.rs
@@ -1,9 +1,11 @@
 //what primitive type is a vector type
 pub fn main () {
+  //types and traits?
   pub wasm_bindgen_futures::JsFuture jsfuture;
   pub std::ptr::null_mut() null;
   pub Result<(), syn_file_expand::Error> resultable;
   pub Vec::<OsString> stringifu;
+  //contructors ()able that begs for arguments, else in ./dev.rs
   pub fn arguments -> stringifu {
     stringifu::from_iter(std::env::args_os());
     if arguments.len() != 2 {
