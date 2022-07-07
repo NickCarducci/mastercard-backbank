6888fb7b6c1a0d2c1784be0aac6c236258d7a1a8
Nick Carducci <nmcarducci@gmail.com>
2022-07-06 23:09:47 -0400

    Update lib.rs

--- src/source/eglue/lib.rs
+++ src/source/eglue/lib.rs
@@ -2,11 +2,15 @@ mod bupkis;
 use crate::bupkis::{console,n,jsfuture,arguments};//'arguments'/stringifu::from_iter(std::env::args_os()) gets rewritten inside deepest scope "apply" thisArg, argsArrayArg
 mod jfmast;
 use crate::jfmast;
+
+use wasm_bindgen_futures::JsFuture;
 //fn ego () -> jsfuture awaitfm(arguments[0]);
 
 #[wasm_bindgen(start)]
 pub fn main () -> jsfuture {
   console!(&<n:?Sized>);//&0:?Sized
+  //impl JFMast for JsFuture;
+  //https://docs.smithy.rs/wasm_bindgen_futures/index.html
   println!("{}",jsfuture(&jfmast(&arguments( (&*&0,) ))) );//"src/source/exec.c"
   //jsfuture(&jfmast(&arguments(n 0)));
 }
