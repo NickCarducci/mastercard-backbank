eccfbbd2f77a5f7e71136aeee9e9ba643c250791
Nick Carducci <nmcarducci@gmail.com>
2022-07-08 12:16:19 -0400

    Update lib.rs

--- src/source/eglue/collagen/lib.rs
+++ src/source/eglue/collagen/lib.rs
@@ -4,16 +4,26 @@ use crate::jfmast;
 
 struct Number(u8);//vec![0]
 //see `../bumkis/null.rs` `src/source/eglue/bumkis/null.rs` for potential `ident` example
-macro_rules! arguments {
+/*macro_rules! arguments {
     ($($args:expr),*) => {{
         $(
-           jfmast( $args );
+           jfmast( $args.iter );
         )*
     }}
-}
+}*/ //https://blog.logrocket.com/macros-in-rust-a-tutorial-with-examples
+//following indicates an unlimited amount of arguments, which call
+macro_rules! array {//"if you need it to be indexable, then you'll want to collect it into a vector."
+    ($args:expr,$($array:tt)*) => { //let y: Vec<_> =
+        //$args.push(..$array.push($($array)*)) (NOOPe)
+         //stackoverflow.com/questions/54607493/what-is-the-rust-equivalent-of-javascripts-spread-operator-for-arrays
+        [$args].iter().chain($($array)*)//.map(|&x|x).collect();
+    }
+}//https://danielkeep.github.io/tlborm/book/pat-incremental-tt-munchers.html
 
 //fn main() -> arguments!(1, 2, "Hello");
 //https://stackoverflow.com/a/57454769/11711280
-fn main ()->arguments!( Number(0));
+//fn main ()->arguments!( Number(0));
 
+fn main () -> jfmast( array!(Number(0)) );
+//fn main () -> jfmast( &array(Number(0)) );
 //fn main () -> jfmast( &arguments(Number(0)) );
