07d113863b419e018b6fb7d5c76e9a76e95c5a3c
Nick Carducci <nmcarducci@gmail.com>
2022-07-08 17:50:39 -0400

    Update jfmast.rs

--- src/source/eglue/collagen/jfmast.rs
+++ src/source/eglue/collagen/jfmast.rs
@@ -3,7 +3,7 @@ use wasm_bindgen::prelude::wasm_bindgen;
 use wasm_bindgen_futures::JsFuture;
 pub struct Jship(Result<JsValue, JsValue>);
 #[wasm_bindgen(start)]
-pub fn jfmast(&args: String) -> Jship {
+pub fn jfmast(&arg: String) -> Jship {
     /* rust has command arguments. the type of Fn are Option<u8,String,etc>
     let args = &arguments();//arguments from scope is made by javascript but not rust.
     let lock = &mods::pathify(&args[0].split("/"));
@@ -17,7 +17,7 @@ pub fn jfmast(&args: String) -> Jship {
     */
 
     fn mastbosun() -> impl Future {
-        async { cc::Build::new().file(lock).expand() = Default::default(&args) };
+        async { cc::Build::new().file(lock).expand() = Default::default(&arg) };
     };
     wasm_bindgen_futures::future_to_promise(mastbosun());
 }
