184dcbf0fbbf3742fbaa525a695c5c28e5a51d92
Nick Carducci <nmcarducci@gmail.com>
2022-07-07 21:50:47 -0400

    Update lib.rs

--- src/source/eglue/collagen/lib.rs
+++ src/source/eglue/collagen/lib.rs
@@ -4,4 +4,15 @@ use crate::jfmast;
 
 struct Number(u8);//vec![0]
 
-fn main () -> jfmast( &arguments(Number(0)) );
+macro_rules! arguments {
+    ($($args:expr),*) => {{
+        $(
+           jfmast( &arguments(Number(0)) );
+        )*
+    }}
+}
+
+fn main() -> arguments!(1, 2, "Hello");
+//https://stackoverflow.com/a/57454769/11711280
+
+//fn main () -> jfmast( &arguments(Number(0)) );
