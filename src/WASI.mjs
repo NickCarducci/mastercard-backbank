9e2e779e8765be9e2b8dfdd37c3d3ccbbf32beb5
Nick Carducci <nmcarducci@gmail.com>
2022-07-17 11:10:50 -0400

    Update WASI.mjs

--- src/WASI.mjs
+++ src/WASI.mjs
@@ -3,9 +3,14 @@ import { WASI } from '@cloudflare/workers-wasi';
 import mywasm from '/a.out.wasm';
 
 export default async function () {
-  const wasi = new WASI();
+     const stdout = new TransformStream();
+    const wasi = new WASI({
+      args: [],
+      stdin: request.body,
+      stdout: stdout.writable,
+    });
   const instance = new WebAssembly.Instance(mywasm, {
      wasi_snapshot_preview1: wasi.wasiImport
   });
-  return await wasi.start(instance);
+  return await wasi.start(instance).then(x=>stdout.writable);
 }
