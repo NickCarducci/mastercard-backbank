d2c28ddb49978c3a37ab9bee1c45f8bbe953ca53
Nick Carducci <nmcarducci@gmail.com>
2022-07-19 09:45:13 -0400

    Update index.rs

--- index.rs
+++ index.rs
@@ -41,7 +41,7 @@ impl DurableObject for DurableObjectExample {
 
     let lock: std::path::PathBuf = pathify("./exec.c");
     let durr: Vec<u8> = cc::Build::new().file(lock).expand();
-    let data: String = String::from_utf8(&durr).unwrap();
+    let data: String = String::from_utf8(durr).unwrap();
     self.data = data;
     return Response::ok(&format!("{} data.", self.data));
   }
