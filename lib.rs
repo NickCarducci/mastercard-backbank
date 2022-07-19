881f73a8783ed025cbad9737cb3c85a08a3be81b
Nick Carducci <nmcarducci@gmail.com>
2022-07-18 20:51:57 -0400

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,11 +1,53 @@
+//https://github.com/cloudflare/workers-rs/issues/94
+//"you'll need to define the Durable Object in a separate module. ...this:"
+//"https://github.com/cloudflare/workers-rs/blob/main/worker-sandbox/src/lib.rs"
+
 
 // We're able to specify a start event that is called when the WASM is initialized before any
 // requests. This is useful if you have some global state or setup code, like a logger. This is
 // only called once for the entire lifetime of the worker.
 #[event(start)]
 pub fn start() {
-    utils::set_panic_hook();
+    //utils::set_panic_hook();
 
     // Change some global state so we know that we ran our setup function.
     //GLOBAL_STATE.store(true, Ordering::SeqCst);
+}
+
+/*fn handle_a_request<D>(req: Request, _ctx: RouteContext<D>) -> Result<Response> {
+    Response::ok(&format!(
+        "req at: {}, located at: {:?}, within: {}",
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
+    ))
+}*/
+
+async fn handle_async_request<D>(req: Request, _ctx: RouteContext<D>) -> Result<Response> {
+    Response::ok(&format!(
+        "[async] req at: {}, located at: {:?}, within: {}",
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
+    ))
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    let data = SomeSharedData {
+        regex: regex::Regex::new(r"^\d{4}-\d{2}-\d{2}$").unwrap(),
+    };
+
+    let router = Router::with_data(data); // if no data is needed, pass `()` or any other valid data
+
+    router
+        .get_async("/durable/:id", |_req, ctx| async move {
+            let namespace = ctx.durable_object("EXAMPLE_CLASS_DURABLE_OBJECT")?;
+            let stub = namespace.id_from_name("A")?.get_stub()?;
+            //https://stackoverflow.com/questions/70309403/updating-html-canvas-imagedata-using-rust-webassembly
+            // when calling fetch to a Durable Object, a full URL must be used. Alternatively, a
+            // compatibility flag can be provided in wrangler.toml to opt-in to older behavior:
+            // https://developers.cloudflare.com/workers/platform/compatibility-dates#durable-object-stubfetch-requires-a-full-url
+            stub.fetch_with_str("https://fake-host/").await
+        })
 }
