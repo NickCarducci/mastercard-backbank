11e39e53b59e29c903991343bea3beb91bd81fff
Nick Carducci <nmcarducci@gmail.com>
2022-07-18 23:14:41 -0400

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -5,12 +5,15 @@
 // We're able to specify a start event that is called when the WASM is initialized before any
 // requests. This is useful if you have some global state or setup code, like a logger. This is
 // only called once for the entire lifetime of the worker.
+
+
+use std::sync::atomic::Ordering;
 #[event(start)]
 pub fn start() {
     //utils::set_panic_hook();
 
     // Change some global state so we know that we ran our setup function.
-    //GLOBAL_STATE.store(true, Ordering::SeqCst);
+    GLOBAL_STATE.store(true, Ordering::SeqCst);
 }
 
 /*fn handle_a_request<D>(req: Request, _ctx: RouteContext<D>) -> Result<Response> {
