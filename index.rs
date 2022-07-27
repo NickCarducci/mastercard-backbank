7444ac341642e95b977c21b5dc0ecf6e281b6aee
Nick Carducci <nmcarducci@gmail.com>
2022-07-27 16:27:44 -0400

    Update index.rs

--- index.rs
+++ index.rs
@@ -4,47 +4,61 @@ use worker::{
 };
 #[durable_object]
 pub struct DurableObjectExample {
-  data: String, //Vec<u8>,
+  //app: String,//Vec<u8>,
   initialized: bool,
   state: State,
   env: Env, // access `Env` across requests, use inside `fetch`
 }
 
-fn pathify(path: &str) -> std::path::PathBuf {
+/*fn pathify(path: &str) -> std::path::PathBuf {
   let mut input_file = std::path::PathBuf::new();
   let _arr: () = path.split("/").map(|x| input_file.push(x)).collect();
   return input_file;
-}
+}*/
 
 #[durable_object]
 impl DurableObject for DurableObjectExample {
   fn new(state: State, env: Env) -> Self {
     Self {
-      data: format!(""), //vec![]//https://www.hackertouch.com/how-to-create-and-check-string-is-empty-rust.html
+      //app: "".to_owned(), //format!(""),vec![]
+      //https://www.hackertouch.com/how-to-create-and-check-string-is-empty-rust.html
       initialized: false,
       state: state,
       env,
     }
   }
   async fn fetch(&mut self, _req: Request) -> Result<Response> {
-    if !self.initialized {
-      self.initialized = true;
-      //self.count = self.state.storage().get("count").await.unwrap_or(0);
-    }
-    let _data = &self.data;
-    //self.count += 10;
-    //self.state.storage().put("count", self.count).await?;
+    let _state = &self.state;
+    let _env = &self.env;
     //if (!_req.url)
     //return R({ response: "abnormal" }, [400, "abnormal", dataHead]);
 
     //let url = new URL(_req.url);
     //let  value = null;
-    let _state = &self.state;
-    let _env = &self.env;
-    let lock: std::path::PathBuf = pathify("./exec.c");
-    let durr: Vec<u8> = cc::Build::new().file(lock).expand();
-    return /*self.data = */Response::ok(String::from_utf8(durr).unwrap());
+    if !self.initialized {
+      self.initialized = true;
+      //self.app = self.state.storage().get("app").await.unwrap_or(0);
+    }
+    return Response::ok("");
+    /*let lock: std::path::PathBuf = pathify("./exec.c");
+    let _app = &self.app;//hardly any use to add the c>php code to the keyvalue storage
+    //self.app: Vec<v8> = extensionfrom cc::Build().expand
+    //self.app: String = String::from_utf8(self.app).unwrap();
+    //self.state.storage().put("app", self.app).await?;
+    let appel: Vec<u8> = cc::Build::new().file(lock).expand();
+    return /*self.data = */Response::ok(String::from_utf8(appel).unwrap());*/
     //self.data = data::to_string();//https://doc.rust-lang.org/std/macro.format.html
     //return Response::ok(&format!("{} data.", self.data));
   }
 }
+//advocates against false advertisement and countersuables
+//lock up the italians::sweaty
+//profits are only left over value than utility is valuation by payment installments
+//inelasticity as left over
+//flip phillips to misery to complementary
+
+//better effort for deflating efficiency than non-accelerating inflation rate of unemployment
+
+//if I cannot say kill russians on facebook but likely cannot donate without being red flagged
+//asylum for conscripts
+//repo credit purchases, inventory no debit in debenture/stock fiduciary... only by royalty gives debenture a different name
